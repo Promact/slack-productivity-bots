23e0d6230aae3a8540e03f19f9680d2fa79ac19f
gourav <gourav@PR2C4R3Y2015>
2016-08-26 17:49:12 +0530

    updated leaveReportGeneration Repository and controllers and updated routes in client side.

--- Slack.Automation/Promact.Core.Repository/ProjectUserCall/ProjectUserCallRepository.cs
+++ Slack.Automation/Promact.Core.Repository/ProjectUserCall/ProjectUserCallRepository.cs
@@ -215,30 +215,6 @@ namespace Promact.Core.Repository.ProjectUserCall
             }
         }
 
-        /// <summary>
-        /// Method to call an api from oauth server and get all the users including in a project using teamleader id
-        /// </summary>
-        /// <param name="teamLeaderId"></param>
-        /// <param name="accessToken"></param>
-        /// <returns>list of users in a project</returns>
-        public async Task<List<User>> GetProjectUsersByTeamLeaderId(string teamLeaderId, string accessToken)
-        {
-            try
-            {
-                List<User> projectUsers = new List<User>();
-                var requestUrl = string.Format("{0}{1}", StringConstant.ProjectUsersByTeamLeaderId, teamLeaderId);
-                var response = await _httpClientRepository.GetAsync(AppSettingsUtil.ProjectUrl, requestUrl, accessToken);
-                var responseContent = response.Content.ReadAsStringAsync().Result;
-                if (response.StatusCode != HttpStatusCode.Forbidden)
-                {
-                     projectUsers = JsonConvert.DeserializeObject<List<User>>(responseContent);
-                }
-                return projectUsers;
-            }
-            catch (System.Exception ex)
-            {
-                throw ex;
-            }
-        }
+
     }
 }
