59655cc8b97955836c49eba0d1613bd9e533aa14
julie <julie@PR1C4R4Y2015>
2016-08-29 11:25:40 +0530

    commited for scrumbotTest change

--- Slack.Automation/Promact.Core.Test/ScrumBotRepositoryTest.cs
+++ Slack.Automation/Promact.Core.Test/ScrumBotRepositoryTest.cs
@@ -21,6 +21,7 @@ namespace Promact.Core.Test
         [Fact, Trait("Category", "Required")]
         public async void ScrumInitiateNoQuestion()
         {
+
             //LeaveRequest leave = new LeaveRequest() { FromDate = DateTime.UtcNow, EndDate = DateTime.UtcNow, Reason = "testing", RejoinDate = DateTime.UtcNow, Status = Condition.Pending, Type = "Casual", CreatedOn = DateTime.UtcNow, EmployeeId = "fc172bd7-42d9-4cbf-baa8-130be02d25ed" };
             //_leaveRequestRepository.ApplyLeave(leave);
             //Assert.NotEqual(51, leave.Id);
