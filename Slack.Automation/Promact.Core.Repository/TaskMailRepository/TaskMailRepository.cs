b7243e02e4e7e1273081fb04f01c88e8a7622920
Ronak <ronak@PR2C4R4Y2015>
2016-09-05 17:22:33 +0530

    Added string constant in TaskMail Repository

--- Slack.Automation/Promact.Core.Repository/TaskMailRepository/TaskMailRepository.cs
+++ Slack.Automation/Promact.Core.Repository/TaskMailRepository/TaskMailRepository.cs
@@ -367,6 +367,11 @@ namespace Promact.Core.Repository.TaskMailRepository
             return emailBody;
         }
 
+        /// <summary>
+        /// Method geting the user/users information 
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <returns></returns>
         public async Task<List<TaskMailReportAc>> TaskMailReport(string userId)
         {
             var user = _user.FirstOrDefault(x => x.Id == userId);
@@ -374,7 +379,7 @@ namespace Promact.Core.Repository.TaskMailRepository
             var Json = await _projectUserRepository.GetUserRole(user.UserName, accessToken);
             var role = Json.FirstOrDefault(x => x.UserName == user.UserName);
             List<TaskMailReportAc> taskMailReportAc = new List<TaskMailReportAc>();
-            if (role.Role == "Admin")
+            if (role.Role == StringConstant.RoleAdmin)
             {
                 List<TaskMail> taskMails = new List<TaskMail>();
                 foreach (var j in Json)
@@ -395,10 +400,8 @@ namespace Promact.Core.Repository.TaskMailRepository
                         });
                     }
                 }
-               // return taskMailReportAc;//.OrderByDescending(o => o.CreatedOn).ToList();
-
             }
-            else if (role.Role == "TeamLeader")
+            else if (role.Role == StringConstant.RoleTeamLeader)
             {
                 List<TaskMail> taskMails = new List<TaskMail>();
                 foreach (var j in Json)
@@ -419,7 +422,7 @@ namespace Promact.Core.Repository.TaskMailRepository
                         });
                     }
                 }
-               // 
+               
             }
             else
             {
@@ -437,11 +440,16 @@ namespace Promact.Core.Repository.TaskMailRepository
                     taskMailReportAc.Add(taskmailReportAc);
                 });
 
-               // return taskMailReportAc.OrderByDescending(o => o.CreatedOn).ToList();
+              
             }
             return taskMailReportAc.OrderByDescending(o => o.CreatedOn).ToList();
         }
 
+        /// <summary>
+        ///Fetches the task mail details of the given id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
         public async Task<List<TaskMailReportAc>> TaskMailDetailsReport(int id)
         {
             List<TaskMailReportAc> taskMailReportAc = new List<TaskMailReportAc>();
--- Slack.Automation/Promact.Erp.Util/StringConstant.cs
+++ Slack.Automation/Promact.Erp.Util/StringConstant.cs
@@ -182,5 +182,9 @@ namespace Promact.Erp.Util
 
         public static string ProjectInformationUrl = "featchUserRole/";
 
+        public static string RoleAdmin = "Admin";
+        public static string RoleTeamLeader = "TeamLeader";
+        public static string RoleEmployee = "Employee";
+
     }
 }
