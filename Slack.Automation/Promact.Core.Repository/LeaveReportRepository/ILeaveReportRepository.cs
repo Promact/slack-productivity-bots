c152bd9c3eb0d0edcdb3b73974fde633da7cd9e5
gourav <gourav@PR2C4R3Y2015>
2016-09-05 19:37:19 +0530

    added comments in ILeaveReportGeneration

--- Slack.Automation/Promact.Core.Repository/LeaveReportRepository/ILeaveReportRepository.cs
+++ Slack.Automation/Promact.Core.Repository/LeaveReportRepository/ILeaveReportRepository.cs
@@ -9,9 +9,21 @@ namespace Promact.Core.Repository.LeaveReportRepository
 {
     public interface ILeaveReportRepository
     {
+        /// <summary>
+        /// Method that returns the list of employees with their leave status based on their roles
+        /// </summary>
+        /// <param name="accessToken"></param>
+        /// <param name="userName"></param>
+        /// <returns>List of employees with leave status based on roles</returns> 
         Task<IEnumerable<LeaveReport>> LeaveReport(string accessToken,string userName);
 
-        Task <IEnumerable<LeaveReportDetails>> LeaveReportDetails(string employeeId, string accessToken);
+        /// <summary>
+        /// Method that returns the details of leave for an employee
+        /// </summary>
+        /// <param name="employeeId"></param>
+        /// <param name="accessToken"></param>
+        /// <returns>Details of leave for an employee</returns>
+        Task<IEnumerable<LeaveReportDetails>> LeaveReportDetails(string employeeId, string accessToken);
         
     }
 }
--- Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport.model.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var LeaveReport = (function () {
-    function LeaveReport() {
-    }
-    return LeaveReport;
-}());
-exports.LeaveReport = LeaveReport;
-//# sourceMappingURL=leaveReport.model.js.map
