c15593deeb3d2ad23ae95880fb1ed87c2632411d
Siddhartha Shaw <siddhartha@promactinfo.com>
2016-09-05 11:22:44 +0530

    Added more test cases in all repository test.cs and modified SlackUserDetails table

--- Slack.Automation/Promact.Core.Test/AttachmentRepositoryTest.cs
+++ Slack.Automation/Promact.Core.Test/AttachmentRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using Autofac;
 using Microsoft.AspNet.Identity;
 using Promact.Core.Repository.AttachmentRepository;
+using Promact.Erp.DomainModel.ApplicationClass;
 using Promact.Erp.DomainModel.Models;
 using Promact.Erp.Util;
 using System;
@@ -26,42 +27,56 @@ namespace Promact.Core.Test
         }
 
         /// <summary>
-        /// Test case to check creating attchment of slack used generically
+        /// Test case to check creating attchment of slack used generically for true value
         /// </summary>
         [Fact, Trait("Category", "Required")]
         public void SlackResponseAttachment()
         {
-            string hello = StringConstant.Hello;
-            var response = _attachmentRepository.SlackResponseAttachment("1", hello).Last();
+            var response = _attachmentRepository.SlackResponseAttachment("1", StringConstant.Hello).Last();
             Assert.Equal(response.Title, StringConstant.Hello);
             Assert.Equal(response.Color, StringConstant.Color);
         }
 
         /// <summary>
-        /// Test case to check creating text corresponding to leave details and user which is to be send on slack as reply
+        /// Test case to check creating text corresponding to leave details and user which is to be send on slack as reply for true value
         /// </summary>
         [Fact, Trait("Category", "Required")]
         public void ReplyText()
         {
-            LeaveRequest leave = new LeaveRequest();
-            leave.Reason = StringConstant.Reason;
+            LeaveRequest leave = new LeaveRequest()
+            {
+                CreatedOn = DateTime.UtcNow,
+                EmployeeId = StringConstant.StringIdForTest,
+                EndDate = DateTime.UtcNow,
+                FromDate = DateTime.UtcNow,
+                Reason = StringConstant.LeaveReasonForTest,
+                RejoinDate = DateTime.UtcNow,
+                Status = Condition.Pending,
+                Type = StringConstant.LeaveTypeForTest,
+                Id = 1
+            };
+            var replyText = string.Format("Leave has been applied by {0} From {1} To {2} for Reason {3} will re-join by {4}",
+                StringConstant.FirstNameForTest,
+                leave.FromDate.ToShortDateString(),
+                leave.EndDate.ToShortDateString(),
+                leave.Reason,
+                leave.RejoinDate.ToShortDateString());
             var response = _attachmentRepository.ReplyText(StringConstant.FirstNameForTest,leave);
-            Assert.NotEqual(response, null);
+            Assert.Equal(response, replyText);
         }
 
         /// <summary>
-        /// Test case to check break string by spaces only if spaces are not between quotes
+        /// Test case to check break string by spaces only if spaces are not between quotes for true value
         /// </summary>
         [Fact, Trait("Category", "Required")]
         public void SlackText()
         {
-            string hello = StringConstant.Hello;
-            var response = _attachmentRepository.SlackText(hello).Last();
+            var response = _attachmentRepository.SlackText(StringConstant.Hello).Last();
             Assert.Equal(response, StringConstant.All);
         }
 
         /// <summary>
-        /// Test cases to check transform NameValueCollection to SlashCommand class
+        /// Test cases to check transform NameValueCollection to SlashCommand class for true value
         /// </summary>
         [Fact, Trait("Category", "Required")]
         public void SlashCommandTransfrom()
@@ -83,6 +98,73 @@ namespace Promact.Core.Test
             var secondResult = _userManager.AddLoginAsync(user.Id, info).Result;
             var accessToken = _attachmentRepository.AccessToken(user.Email).Result;
             Assert.Equal(accessToken, StringConstant.AccessTokenForTest);
+        }
+
+        /// <summary>
+        /// Test case to check creating attchment of slack used generically for false value
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void SlackResponseAttachmentFalse()
+        {
+            var response = _attachmentRepository.SlackResponseAttachment("55", StringConstant.Hello).Last();
+            Assert.NotEqual(response.Title, StringConstant.FirstNameForTest);
+        }
+
+        /// <summary>
+        /// Test case to check creating text corresponding to leave details and user which is to be send on slack as reply for false value
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void ReplyTextFalse()
+        {
+            LeaveRequest leave = new LeaveRequest()
+            {
+                CreatedOn = DateTime.UtcNow,
+                EmployeeId = StringConstant.StringIdForTest,
+                EndDate = DateTime.UtcNow,
+                FromDate = DateTime.UtcNow,
+                Reason = StringConstant.LeaveReasonForTest,
+                RejoinDate = DateTime.UtcNow,
+                Status = Condition.Pending,
+                Type = StringConstant.LeaveTypeForTest,
+                Id = 1
+            };
+            var replyText = string.Format("Leave has been applied by {0} From {1} To {2} for Reason {3} will re-join by {4}",
+                StringConstant.FirstNameForTest,
+                leave.FromDate.ToShortDateString(),
+                leave.EndDate.ToShortDateString(),
+                leave.Reason,
+                leave.RejoinDate.ToShortDateString());
+            leave.Reason = StringConstant.Reason;
+            var response = _attachmentRepository.ReplyText(StringConstant.FirstNameForTest, leave);
+            Assert.NotEqual(response, replyText);
+        }
+
+        /// <summary>
+        /// Test case to check break string by spaces only if spaces are not between quotes for false value
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void SlackTextFalse()
+        {
+            var response = _attachmentRepository.SlackText(StringConstant.Hello).Last();
+            Assert.NotEqual(response, StringConstant.FirstNameForTest);
+        }
+
+        /// <summary>
+        /// Test case to check Method AccessToken of Attachment Repository for false value
+        /// </summary>
+        [Fact, Trait("Category", "Required")]
+        public void AccessTokenFalse()
+        {
+            var firstUser = new ApplicationUser() { Email = StringConstant.EmailForTest, UserName = StringConstant.EmailForTest, SlackUserName = StringConstant.FirstNameForTest };
+            var secondUser = new ApplicationUser() { Email = StringConstant.TeamLeaderEmailForTest, UserName = StringConstant.TeamLeaderEmailForTest, SlackUserName = StringConstant.LastNameForTest };
+            var result = _userManager.CreateAsync(firstUser).Result;
+            result = _userManager.CreateAsync(secondUser).Result;
+            UserLoginInfo firstInfo = new UserLoginInfo(StringConstant.PromactStringName, StringConstant.AccessTokenForTest);
+            UserLoginInfo secondInfo = new UserLoginInfo(StringConstant.PromactStringName, StringConstant.SlackChannelIdForTest);
+            result = _userManager.AddLoginAsync(firstUser.Id, firstInfo).Result;
+            result = _userManager.AddLoginAsync(secondUser.Id, secondInfo).Result;
+            var accessToken = _attachmentRepository.AccessToken(secondUser.Email).Result;
+            Assert.NotEqual(accessToken, StringConstant.AccessTokenForTest);
         }
     }
 }
