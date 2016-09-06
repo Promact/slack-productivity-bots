1841888d1330a9f9eaaf1e5a17aa6add66b4be8a
julie <julie@PR1C4R4Y2015>
2016-09-06 10:20:47 +0530

    committing change in ScrumBotRepositoryTest

--- Slack.Automation/Promact.Core.Test/ScrumBotRepositoryTest.cs
+++ Slack.Automation/Promact.Core.Test/ScrumBotRepositoryTest.cs
@@ -103,7 +103,7 @@ namespace Promact.Core.Test
             var userRequestUrl = string.Format("{0}{1}", StringConstant.UsersDetailByGroupUrl, StringConstant.GroupName);
             _mockHttpClient.Setup(x => x.GetAsync(StringConstant.ProjectUrl, userRequestUrl, StringConstant.AccessTokenForTest)).Returns(userResponse);
 
-            // _slackUserRepository.AddSlackUser(slackUserDetails);
+            _slackUserRepository.AddSlackUser(slackUserDetails);
             UserLoginInfo info = new UserLoginInfo(StringConstant.PromactStringName, StringConstant.AccessTokenForTest);
             await _userManager.CreateAsync(user);
             await _userManager.AddLoginAsync(user.Id, info);
