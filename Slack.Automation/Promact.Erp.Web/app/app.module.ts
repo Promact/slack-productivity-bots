d601468a55a6e455c020fb8a046ede9f1c6fc389
gourav <gourav@PR2C4R3Y2015>
2016-09-14 19:28:28 +0530

    added scrum report controller and repositories and application classes and client side views for the same.

--- Slack.Automation/Promact.Erp.Web/app/app.module.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.module.ts
@@ -16,6 +16,21 @@ import { LeaveModule } from './leaveReport/leaveReport.module';
 import { ScrumModule } from './ScrumReport/scrumReport.module';
 
 
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { routing }       from './app.routes';
+import { LeaveReportComponent } from './leaveReport/leaveReport.component';
+import { LeaveReportListComponent } from './leaveReport/leaveReport-List/leaveReport-List.component';
+import { LeaveReportDetailsComponent } from './leaveReport/leaveReport-Details/leaveReport-Details.component';
+import { LeaveReportService } from './leaveReport/leaveReport.service';
+import { ScrumReportComponent } from './ScrumReport/scrumReport.component';
+import { ScrumReportService } from './ScrumReport/scrumReport.service';
+import { ScrumProjectListComponent } from './ScrumReport/scrumProject-List/scrumProject-List.component';
+import { ScrumProjectDetailComponent } from './ScrumReport/scrumProject-Details/scrumProject-Details.component';
+
+
+
 @NgModule({
     declarations: [AppComponent],
     imports: [
@@ -26,6 +41,7 @@ import { ScrumModule } from './ScrumReport/scrumReport.module';
         LeaveModule,
         ScrumModule
     ],
+    declarations: [AppComponent, LeaveReportComponent, LeaveReportListComponent, LeaveReportDetailsComponent, ScrumReportComponent, ScrumProjectListComponent, ScrumProjectDetailComponent],
     bootstrap: [AppComponent],
     providers: [StringConstant, LoaderService,{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
