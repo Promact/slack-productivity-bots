2b645e01414ac47e7305410f602412070be721e0
gourav <gourav@PR2C4R3Y2015>
2016-09-14 19:28:28 +0530

    added scrum report controller and repositories and application classes and client side views for the same.

--- Slack.Automation/Promact.Erp.Web/app/app.module.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.module.ts
@@ -14,6 +14,21 @@ import { LoaderService } from "./shared/loader.service";
 import { TaskMailModule } from './taskmail/taskMail.module';
 import { LeaveModule } from './leaveReport/leaveReport.module';
 
+import { FormsModule }   from '@angular/forms';
+import { AppComponent }  from './app.component';
+import { HttpModule, JsonpModule } from '@angular/http';
+import { routing }       from './app.routes';
+import { LeaveReportComponent } from './leaveReport/leaveReport.component';
+import { LeaveReportListComponent } from './leaveReport/leaveReport-List/leaveReport-List.component';
+import { LeaveReportDetailsComponent } from './leaveReport/leaveReport-Details/leaveReport-Details.component';
+import { LeaveReportService } from './leaveReport/leaveReport.service';
+import { ScrumReportComponent } from './ScrumReport/scrumReport.component';
+import { ScrumReportService } from './ScrumReport/scrumReport.service';
+import { ScrumProjectListComponent } from './ScrumReport/scrumProject-List/scrumProject-List.component';
+import { ScrumProjectDetailComponent } from './ScrumReport/scrumProject-Details/scrumProject-Details.component';
+
+
+
 @NgModule({
     declarations: [AppComponent],
     imports: [
@@ -23,6 +38,7 @@ import { LeaveModule } from './leaveReport/leaveReport.module';
         TaskMailModule,
         LeaveModule
     ],
+    declarations: [AppComponent, LeaveReportComponent, LeaveReportListComponent, LeaveReportDetailsComponent, ScrumReportComponent, ScrumProjectListComponent, ScrumProjectDetailComponent],
     bootstrap: [AppComponent],
     providers: [StringConstant, LoaderService,{ provide: LocationStrategy, useClass: HashLocationStrategy }]
 })
--- Slack.Automation/Promact.Erp.Web/app/app.routes.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.routes.ts
@@ -1,6 +1,8 @@
 ﻿import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LeaveReportComponent } from './leaveReport/leaveReport.component';
+import { ScrumReportComponent } from './ScrumReport/scrumReport.component';
+import { ScrumReportRoutes } from './ScrumReport/scrumReport.routes';
 
 
 
@@ -9,3 +11,19 @@ const appRoutes: Routes =
        { path: '', component: LeaveReportComponent }
     ]
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
+
+
+const appRoutes: Routes = [
+    { path: 'Home/AfterLogIn', component: LeaveReportComponent },
+    ...LeaveReportRoutes,
+    { path: 'leave', component: LeaveReportComponent },
+    ...ScrumReportRoutes,
+    { path: 'scrum', component: ScrumReportComponent }
+        //...TaskMailRoutes,
+    //{ path: 'task', component: TaskMailComponent },
+
+];
+
+
+
+export const routing = RouterModule.forRoot(appRoutes);
