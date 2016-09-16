7252e751e714fabb185bd4433b52253e057d2d69
gourav <gourav@PR2C4R3Y2015>
2016-09-16 11:57:45 +0530

    modified scrum repository to return scrum answers in  form of an array

---
+++ Slack.Automation/Promact.Core.Test/ScrumReportRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Moq;
+using Promact.Core.Repository.HttpClientRepository;
+using Promact.Core.Repository.ScrumReportRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promact.Core.Test
+{
+    public class ScrumReportRepositoryTest
+    {
+        private IComponentContext _componentContext;
+        private IScrumReportRepository _scrumReportRepository;
+        private readonly Mock<IHttpClientRepository> _mockHttpClient;
+
+        public ScrumReportRepositoryTest()
+        {
+            _componentContext = AutofacConfig.RegisterDependancies();
+            _scrumReportRepository = _componentContext.Resolve<IScrumReportRepository>();
+            _mockHttpClient = _componentContext.Resolve<Mock<IHttpClientRepository>>();
+        }
+
+
+    }
+}
