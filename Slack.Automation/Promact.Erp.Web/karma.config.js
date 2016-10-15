12ae75abf64ba77474274953dfbfedb86c5f7540
ankit <ankit@PR1C2R4Y2015>
2016-10-15 14:19:51 +0530

    update karma.config.js and package.json

--- Slack.Automation/Promact.Erp.Web/karma.config.js
+++ Slack.Automation/Promact.Erp.Web/karma.config.js
@@ -59,11 +59,11 @@ module.exports = function (config) {
         ],
 
 
-        // preprocess matching files before serving them to the browser
-        // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        //preprocessors: {
-        //    "**/app/*spec.js": "coverage"
-        //},
+    // preprocess matching files before serving them to the browser
+    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
+    preprocessors: {
+         '**/app/**/!(*spec).js': ['coverage']
+    },
 
         // test results reporter to use
         // possible values: 'dots', 'progress'
