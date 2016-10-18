6cb0e6ea032f00d88ba4f7bc5da0b281e8051047
Ronak <ronak@PR2C4R4Y2015>
2016-10-18 18:00:04 +0530

    Added loder in login.cshtml and updated taksmail-details component and taskmail-list component

--- Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-details/taskmail-details.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-details/taskmail-details.component.ts
@@ -68,7 +68,7 @@ export class TaskMailDetailsComponent {
                 this.loader.loader = false;
             });
         });
-       this.loader.loader = false;
+      
     }
     getTaskMailList() {
         this.router.navigate([this.stringConstant.taskList]);
