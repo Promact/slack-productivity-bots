514c8f2af5e2c6667073da4a6bfc21077b9a8855
julie <julie@PR2C4R3Y2015>
2016-10-18 14:48:12 +0530

    description added in SlackAuthorizeException.cs

--- Slack.Automation/Promact.Erp.Util/SlackAuthorizeException.cs
+++ Slack.Automation/Promact.Erp.Util/SlackAuthorizeException.cs
@@ -4,17 +4,30 @@ namespace Promact.Erp.Util
 {
     public class SlackAuthorizeException : Exception
     {
+        /// <summary>
+        ///  Initializes a new instance of the SlackAuthorizeException class.
+        /// </summary>
         public SlackAuthorizeException()
         {
         }
 
+        /// <summary>
+        ///    Initializes a new instance of the SlackAuthorizeException class with a specified error message.
+        /// </summary>
+        /// <param name="message">error message</param>
         public SlackAuthorizeException(string message)
         : base(message)
         {
         }
 
-        public SlackAuthorizeException(string message, Exception inner)
-        : base(message, inner)
+
+        /// <summary>
+        /// Initializes a new instance of the SlackAuthorizeException class with a specified error message and a reference to the inner exception that is the cause of this exception.        
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="innerException"></param>
+        public SlackAuthorizeException(string message, Exception innerException)
+        : base(message, innerException)
         {
         }
     }
