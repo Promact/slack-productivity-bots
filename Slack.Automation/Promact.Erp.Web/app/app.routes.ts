8e1989da785fbd90028533b7e1ea734d7375785a
Ronak <ronak@PR2C4R4Y2015>
2016-11-08 17:41:11 +0530

    Updated app.routes.ts and  Fixes #72

--- Slack.Automation/Promact.Erp.Web/app/app.routes.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.routes.ts
@@ -2,13 +2,7 @@
 import { Routes, RouterModule } from '@angular/router';
 import { LeaveReportComponent } from './leaveReport/leaveReport.component';
 
-const appRoutes: Routes = [
-    { path: '', component: LeaveReportComponent },
-    { path: 'Home/AfterLogIn', component: LeaveReportComponent },
-    ...LeaveReportRoutes,
-    { path: 'leave', component: LeaveReportComponent },
-     ...TaskMailRoutes,
-    { path: 'task', component: TaskMailComponent },
+
 
 const appRoutes: Routes =
     [
