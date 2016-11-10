134912781aa71e8f602a9eb1e0bd5fee8d6e2cc2
Gourav Agarwal <gourav@promactinfo.com>
2016-11-10 15:15:20 +0530

    updated the project to angular 2.1.1 and solved the routing issues developed by changing the url patterns.

--- Slack.Automation/Promact.Erp.Web/app/app.routes.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.routes.ts
@@ -1,8 +1,6 @@
 ﻿import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LeaveReportComponent } from './leaveReport/leaveReport.component';
-import { ScrumReportComponent } from './ScrumReport/scrumReport.component';
-import { ScrumReportRoutes } from './ScrumReport/scrumReport.routes';
 
 const appRoutes: Routes =
     [
