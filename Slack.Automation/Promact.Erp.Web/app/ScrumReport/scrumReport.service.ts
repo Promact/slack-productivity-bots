6b0f8661dbd1c8af029c53a238bb5b8ba48ef8d6
Gourav Agarwal <gourav@promactinfo.com>
2016-11-10 15:15:20 +0530

    updated the project to angular 2.1.1 and solved the routing issues developed by changing the url patterns.

--- Slack.Automation/Promact.Erp.Web/app/ScrumReport/scrumReport.service.ts
+++ Slack.Automation/Promact.Erp.Web/app/ScrumReport/scrumReport.service.ts
@@ -12,18 +12,29 @@ export class ScrumReportService {
    
     constructor(private http: Http, private stringConstant: StringConstant) { }
 
+     /*This method is used to get list of projects*
+     * 
+     */
     getScrumProjects(): Observable<ScrumProject[]> {
         return this.http.get(this.stringConstant.scrumProjects)
             .map(res => res.json())
             .catch(this.handleError);
     }
 
+    /*This method is used to get scrumdetails for a particular project on a specific date*
+     * 
+     * @param Id
+     * @param Date
+     */
     getScrumDetails(Id: number, Date: any): Observable<ScrumDetails> {
-        return this.http.get(this.stringConstant.scrumDetails + Id + this.stringConstant.slash + Date)
+        return this.http.get((this.stringConstant.scrumDetails + this.stringConstant.slash + Id + this.stringConstant.slash + Date))
             .map(res => res.json())
             .catch(this.handleError);       
     }
 
+    /*This method is used to handle errors
+    *
+    */
     private handleError(error: any) {
         let errMsg = this.stringConstant.serverError;
         return Observable.throw(errMsg);
