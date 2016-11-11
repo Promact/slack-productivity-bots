db6bdb74f7626f848881dd6f68ee14ec22f9582b
Gourav Agarwal <gourav@promactinfo.com>
2016-11-11 15:58:35 +0530

    changes in apidoc and added a message  to show if no projects are present in list

--- Slack.Automation/Promact.Erp.Web/app/ScrumReport/scrumProject-List/scrumProject-List.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/ScrumReport/scrumProject-List/scrumProject-List.component.ts
@@ -1,8 +1,8 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
 import { ScrumProject } from './scrumProject-List.model';
 import { ScrumReportService } from '../scrumReport.service';
+import { StringConstant } from '../../shared/stringConstant';
 
 @Component({
     templateUrl: './app/ScrumReport/scrumProject-List/scrumProject-List.html',
@@ -11,17 +11,30 @@ import { ScrumReportService } from '../scrumReport.service';
 export class ScrumProjectListComponent implements OnInit {
     scrumProjects: ScrumProject[];
     errorMessage: any;
+    noProject: any;
 
-    constructor(private scrumReportService: ScrumReportService, private router: Router) { }
+    constructor(private scrumReportService: ScrumReportService, private router: Router, private stringConstant: StringConstant) { }
 
     ngOnInit() {
         this.getScrumProjects();
     }
 
     getScrumProjects() {
+        
         this.scrumReportService.getScrumProjects()
             .subscribe(
-            scrumProjects => this.scrumProjects = scrumProjects,
+            scrumProjects => {
+                this.scrumProjects = scrumProjects;
+                if (scrumProjects.length != 0)
+                {
+                    return scrumProjects;
+                }
+                else
+                {
+                    this.noProject = this.stringConstant.noProjectToDisplay;
+                    return this.noProject;
+                }                    
+            },
             error => this.errorMessage = <any>error
         );
     }
