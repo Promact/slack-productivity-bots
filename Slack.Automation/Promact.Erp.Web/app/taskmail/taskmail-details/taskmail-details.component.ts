2998345aa9cc869488cf97ae6a96437fcc5893b4
Ronak <ronak.shah@promactinfo.com>
2016-11-18 10:31:06 +0530

    solved tslintIssues and improve naming convention.

--- Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-details/taskmail-details.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-details/taskmail-details.component.ts
@@ -27,10 +27,9 @@ export class TaskMailDetailsComponent implements OnInit {
     public isMaxDate: string;
     public isMinDate: string;
     public isHide: boolean;
-    
-    constructor(private route: ActivatedRoute, private router: Router, private taskService: TaskService,
-        private stringConstant: StringConstant,private loader: LoaderService) {
-        this.taskMails = new Array<taskmailModel>();
+    //public UserEmail: string;
+    constructor(private route: ActivatedRoute, private router: Router, private taskService: TaskService, private spinner: SpinnerService, private stringConstant: StringConstant) {
+        this.taskMailDetails = new Array<TaskMailDetailsModel>();
 
     }
     ngOnInit() {
@@ -45,8 +44,12 @@ export class TaskMailDetailsComponent implements OnInit {
             this.UserId = params['UserId']; 
             this.UserRole = params['UserRole'];
             this.UserName = params['UserName'];
-            if (this.UserRole === this.stringConstant.RoleAdmin)
-            { this.isHide = false; } else { this.isHide = true; }
+            if (this.UserRole === this.stringConstant.RoleAdmin) {
+                this.isHide = false;
+            }
+            else {
+                this.isHide = true;
+            }
             this.isMax = true;
             this.taskService.getTaskMailDetailsReport(this.UserId, this.UserRole, this.UserName).subscribe(taskMailUser => {
                 this.taskMail = taskMailUser;
@@ -80,8 +83,8 @@ export class TaskMailDetailsComponent implements OnInit {
     getTaskMailPrevious(UserName, UserId, UserRole, CreatedOn) {
         this.SelectedDate = "";
        this.taskService.getTaskMailDetailsReportPreviousDate(UserName,UserId,UserRole, CreatedOn).subscribe(taskMailUser => {
-           this.taskMailUser = taskMailUser;
-                if (this.taskMailUser[0].IsMin === this.taskMailUser[0].CreatedOn) {
+           this.taskMail = taskMailUser;
+           if (this.taskMail[0].IsMin === this.taskMail[0].CreatedOn) {
                     this.isMin = true;
                 }
            this.taskMail.forEach(taskmailuser => {
@@ -102,8 +105,8 @@ export class TaskMailDetailsComponent implements OnInit {
     getTaskMailNext(UserName, UserId, UserRole, CreatedOn) {
         this.SelectedDate = "";
         this.taskService.getTaskMailDetailsReportNextDate(UserName, UserId, UserRole, CreatedOn).subscribe(taskMailUser => {
-            this.taskMailUser = taskMailUser;
-            if (this.taskMailUser[0].IsMax === this.taskMailUser[0].CreatedOn) {
+            this.taskMail = taskMailUser;
+            if (this.taskMail[0].IsMax === this.taskMail[0].CreatedOn) {
                 this.isMax = true;
             }
             this.isMin = false;
@@ -124,12 +127,12 @@ export class TaskMailDetailsComponent implements OnInit {
     }
     getTaskMailForSelectedDate(UserName, UserId, UserRole, CreatedOn, SelectedDate) {
         this.taskService.getTaskMailDetailsReportSelectedDate(UserName, UserId, UserRole, CreatedOn, SelectedDate).subscribe(taskMailUser => {
-            this.taskMailUser = taskMailUser;
-            if (this.taskMailUser[0].IsMax === this.taskMailUser[0].CreatedOn) {
+            this.taskMail = taskMailUser;
+            if (this.taskMail[0].IsMax === this.taskMail[0].CreatedOn) {
                 this.isMax = true;
                 this.isMin = false;
             }
-            if (this.taskMailUser[0].IsMin === this.taskMailUser[0].CreatedOn) {
+            if (this.taskMail[0].IsMin === this.taskMail[0].CreatedOn) {
                 this.isMax = false;
                 this.isMin = true;
             }
@@ -137,8 +140,9 @@ export class TaskMailDetailsComponent implements OnInit {
                 let datePipe = new DatePipe("medium");
                 taskmailuser.CreatedOns = datePipe.transform(taskmailuser.CreatedOn, this.stringConstant.dateFormat);
                 taskmailuser.TaskMails.forEach(taskMail => {
-                    if (taskMail.Comment === this.stringConstant.notAvailableComment)
-                    { taskMail.StatusName = this.stringConstant.notAvailableComment }
+                    if (taskMail.Comment === this.stringConstant.notAvailableComment) {
+                        taskMail.StatusName = this.stringConstant.notAvailableComment;
+                    }
                     else {
                         taskMail.StatusName = TaskMailStatus[taskMail.Status];
                     }
--- Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-list/taskmail-list.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail-list/taskmail-list.component.ts
@@ -1,19 +1,20 @@
 ﻿import { Component, OnInit } from '@angular/core';
 import {Router, ActivatedRoute } from '@angular/router';
 import { TaskService }   from '../taskmail.service';
-import {taskmailuserModel} from '../taskmailuser.model';
-import {TaskMailStatus} from '../../enums/TaskMailStatus';
-import { LoaderService } from '../../shared/loader.service';
+
+import { TaskMailModel } from '../taskmail.model';
+
+import { TaskMailStatus } from '../../enums/TaskMailStatus';
+import { SpinnerService } from '../../shared/spinner.service';
 import {StringConstant} from '../../shared/stringConstant';
 
 @Component({
     templateUrl: "app/taskmail/taskmail-list/taskmail-list.html",
     providers: [StringConstant]
 })
-export class TaskMailListComponent {
-    listOfUsers: any;
-    constructor(private router: Router, private taskService: TaskService,
-        private stringConstant: StringConstant,private loader: LoaderService) {
+export class TaskMailListComponent implements OnInit {
+    taskMailUsers: Array<TaskMailModel>;
+    constructor(private router: Router, private taskService: TaskService, private spinner: SpinnerService, private stringConstant: StringConstant) {
 
     }
 
@@ -22,28 +23,27 @@ export class TaskMailListComponent {
         this.getListOfEmployee();
         
     }
-    getListOfEmployee()
-    {
-        this.loader.loader = true;
+    getListOfEmployee() {
+
         this.taskService.getListOfEmployee().subscribe((result) => {
 
             if (result.length > 0)
                 if (result[0].UserRole === this.stringConstant.RoleAdmin) {
-                    this.listOfUsers = result;
+                    this.taskMailUsers = result;
                 }
                 else if (result[0].UserRole === this.stringConstant.RoleTeamLeader) {
-                    var UserId = result[0].UserId;
-                    var UserRole = result[0].UserRole;
-                    var UserName = result[0].UserName;
-                    var UserEmail = result[0].UserEmail;
-                    this.router.navigate([this.stringConstant.taskDetails, UserId, UserRole, UserName]);
+                    let UserId = result[0].UserId;
+                    let UserRole = result[0].UserRole;
+                    let UserName = result[0].UserName;
+                    let UserEmail = result[0].UserEmail;
+                    this.router.navigate([this.stringConstant.taskDetails, UserId, UserRole, UserName]);//]);//, UserEmail]);
                 }
                 else {
-                    var UserId = result[0].UserId;
-                    var UserRole = result[0].UserRole;
-                    var UserName = result[0].UserName;
-                    var UserEmail = result[0].UserEmail;
-                    this.router.navigate([this.stringConstant.taskDetails, UserId, UserRole, UserName]);
+                    let UserId = result[0].UserId;
+                    let UserRole = result[0].UserRole;
+                    let UserName = result[0].UserName;
+                    let UserEmail = result[0].UserEmail;
+                    this.router.navigate([this.stringConstant.taskDetails, UserId, UserRole, UserName]);//]);//,UserEmail]);
                 }
             this.loader.loader = false;
         }, err => {
