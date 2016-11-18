c5de7825a5bb2cbd0c22cb001432e0fa2eca9d29
Ronak <ronak.shah@promactinfo.com>
2016-11-18 15:34:16 +0530

    Improve Naming Convention and Fixes #101

--- Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail.service.ts
+++ Slack.Automation/Promact.Erp.Web/app/taskmail/taskmail.service.ts
@@ -1,8 +1,6 @@
 ﻿import { Injectable } from '@angular/core';
-//import {HttpService} from "../http.service";
-//import 'rxjs/add/operator/toPromise';
-import { taskmailModel } from './taskmail.model';
-import {taskmailuserModel} from './taskmailuser.model';
+import { TaskMailDetailsModel } from './taskmaildetails.model';
+import { TaskMailModel } from './taskmail.model';
 import {Http, Headers, RequestOptions, Response} from "@angular/http";
 import { Observable } from 'rxjs/Rx';
 
@@ -11,18 +9,17 @@ export class TaskService {
     private TaskMailUrl = 'api/TaskReport';  // URL to web api
     constructor(private http: Http) { }
     
-    getListOfEmployee(): Observable<taskmailuserModel[]> {
+    getListOfEmployee(): Observable<TaskMailModel[]> {
         return this.http.get("getAllEmployee/")
             .map(this.extractData)
             .catch(this.handleError);
     }
-    getTaskMailReport(currentPage: number,itemsPerPage:number): Observable<taskmailModel[]> {
-        //return this.httpService.get("taskMailReport");
-        return this.http.get("taskMailReport/" + currentPage + "/" + itemsPerPage)
+    getTaskMailReport(currentPage: number, itemsPerPage: number): Observable<TaskMailDetailsModel[]> {
+       return this.http.get("taskMailReport/" + currentPage + "/" + itemsPerPage)
             .map(this.extractData)
             .catch(this.handleError);
     }
-    getTaskMailDetailsReport(UserId: string, UserRole: string, UserName: string): Observable<taskmailuserModel[]> {//, UserName: string, UserEmail: string): Observable<taskmailuserModel[]> {
+    getTaskMailDetailsReport(UserId: string, UserRole: string, UserName: string): Observable<TaskMailModel[]> {//, UserName: string, UserEmail: string): Observable<taskmailuserModel[]> {
         //return this.httpService.get("taskMailDetailsReport/"+ id);
         return this.http.get("taskMailDetailsReport/" + UserId + "/" + UserRole + "/" + UserName)
             .map(this.extractData)
@@ -30,18 +27,18 @@ export class TaskService {
     }
 
 
-    getTaskMailDetailsReportPreviousDate(UserName: string,UserId: string, UserRole: string, CreatedOns: string): Observable<taskmailuserModel[]> {
+    getTaskMailDetailsReportPreviousDate(UserName: string, UserId: string, UserRole: string, CreatedOns: string): Observable<TaskMailModel[]> {
         return this.http.get("taskMailDetailsReportPreviousDate/" + UserRole + "/" + CreatedOns + "/" + UserId + "/" + UserName)
             .map(this.extractData)
             .catch(this.handleError);
     }
-    getTaskMailDetailsReportNextDate(UserName: string,UserId: string, UserRole: string, CreatedOns: string): Observable<taskmailuserModel[]> {
+    getTaskMailDetailsReportNextDate(UserName: string, UserId: string, UserRole: string, CreatedOns: string): Observable<TaskMailModel[]> {
         return this.http.get("taskMailDetailsReportNextDate/" + UserRole + "/" + CreatedOns + "/" + UserId + "/" + UserName)
             .map(this.extractData)
             .catch(this.handleError);
     }
 
-    getTaskMailDetailsReportSelectedDate(UserName: string, UserId: string, UserRole: string, CreatedOns: string, SelectedDate: string): Observable<taskmailuserModel[]> {
+    getTaskMailDetailsReportSelectedDate(UserName: string, UserId: string, UserRole: string, CreatedOns: string, SelectedDate: string): Observable<TaskMailModel[]> {
         return this.http.get("taskMailDetailsReportSelectedDate/" + UserRole + "/" + CreatedOns + "/" + UserId + "/" + UserName + "/" + SelectedDate)
             .map(this.extractData)
             .catch(this.handleError);
