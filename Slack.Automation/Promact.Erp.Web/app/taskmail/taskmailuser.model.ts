3caeae701fe3381cf5b321b64b2a7dc08e242552
Ronak <ronak.shah@promactinfo.com>
2016-11-23 12:38:23 +0530

    Refactor Task Mail Module and Fixes #105

---
+++ Slack.Automation/Promact.Erp.Web/app/taskmail/taskmailuser.model.ts
@@ -0,0 +1,12 @@
+﻿import { taskmailModel } from './taskmail.model';
+export class taskmailuserModel {
+    UserName: string;
+    UserId: string;
+    UserRole: string;
+    UserEmail: string;
+    CreatedOn: Date;
+    CreatedOns: string;
+    MaxDate: Date;
+    MinDate: Date;
+    TaskMails: Array<taskmailModel>;
+}
