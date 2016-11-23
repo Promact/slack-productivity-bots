16112acf9387b46e4aadc9f78f2e677247b85d81
Ronak <ronak.shah@promactinfo.com>
2016-11-23 17:53:34 +0530

    Added Authorize

--- Slack.Automation/Promact.Erp.Core/Controllers/LeaveReportController.cs
+++ Slack.Automation/Promact.Erp.Core/Controllers/LeaveReportController.cs
@@ -7,6 +7,7 @@ using System.Web.Http;
 
 namespace Promact.Erp.Core.Controllers
 {
+    [Authorize]
     [RoutePrefix("api/leaveReport")]
     [Authorize]
     public class LeaveReportController : WebApiBaseController
