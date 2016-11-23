775048c94771f1d4d99db1834669fbdfbbba79aa
Ronak <ronak.shah@promactinfo.com>
2016-11-23 12:38:23 +0530

    Refactor Task Mail Module and Fixes #105

---
+++ Slack.Automation/Promact.Erp.DomainModel/ApplicationClass/TaskMailDetailReportAc.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promact.Erp.DomainModel.ApplicationClass
+{
+    public class TaskMailDetailReportAc
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserId { get; set; }
+        public string UserRole { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public string Description { get; set; }
+        public string Comment { get; set; }
+        public decimal Hours { get; set; }
+        public int TotalItems { get; set;}
+        public TaskMailStatus Status { get; set; }
+
+        
+    }
+}
