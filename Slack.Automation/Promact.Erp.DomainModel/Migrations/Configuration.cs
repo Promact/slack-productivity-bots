8e415c54cdeb6b3ad8b48829959601a96fa61a9c
Siddhartha Shaw <siddhartha@promactinfo.com>
2016-11-28 14:07:08 +0530

    Configuration.cs - Grammatical error sorted

--- Slack.Automation/Promact.Erp.DomainModel/Migrations/Configuration.cs
+++ Slack.Automation/Promact.Erp.DomainModel/Migrations/Configuration.cs
@@ -25,10 +25,10 @@ namespace Promact.Erp.DomainModel.Migrations
                 Question thirdQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 3, QuestionStatement = "What is the Status of this task?", Type = 2 };
                 Question fourthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 4, QuestionStatement = "Mention if any Comment/Roadblock.", Type = 2 };
                 Question fifthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 5, QuestionStatement = "Do you want to send your task mail?", Type = 2 };
-                Question sixthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 6, QuestionStatement = "Are you sure to send mail? After sending email you won't be able to add any task for today.", Type = 2 };
+                Question sixthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 6, QuestionStatement = "Are you sure to send mail? After sending an email you won't be able to add any task for today.", Type = 2 };
                 Question seventhQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 7, QuestionStatement = "Task Mail Complete", Type = 2 };
                 Question firstQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 1, QuestionStatement = "What did you do to change the world yesterday?", Type = 1 };
-                Question secondQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 2, QuestionStatement = "How you are going to rock it today?", Type = 1 };
+                Question secondQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 2, QuestionStatement = "How are you going to rock it today?", Type = 1 };
                 Question thirdQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 3, QuestionStatement = "Are there any obstacles unfortunate enough to be standing in your way?", Type = 1 };
                 context.Question.AddOrUpdate(x => x.Id,
                     firstQuestionTaskMail,
