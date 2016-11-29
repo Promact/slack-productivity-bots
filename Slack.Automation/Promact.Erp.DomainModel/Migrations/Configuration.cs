ad98b64e0e4df34de7e97db1c220024273834ebb
Siddhartha Shaw <siddhartha@promactinfo.com>
2016-11-29 10:37:35 +0530

    Confirguration.cs - Update question of task mail and scrum bot. Fixes #110

--- Slack.Automation/Promact.Erp.DomainModel/Migrations/Configuration.cs
+++ Slack.Automation/Promact.Erp.DomainModel/Migrations/Configuration.cs
@@ -20,12 +20,12 @@ namespace Promact.Erp.DomainModel.Migrations
         {
             if (context.Question.Count() == 0)
             {
-                Question firstQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 1, QuestionStatement = "On which task did you work on today?", Type = 2 };
-                Question secondQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 2, QuestionStatement = "How many hours have you spent on the task?", Type = 2 };
-                Question thirdQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 3, QuestionStatement = "What is the status of the task?", Type = 2 };
+                Question firstQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 1, QuestionStatement = "On which task did you work today?", Type = 2 };
+                Question secondQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 2, QuestionStatement = "How many hours have you spent on task?", Type = 2 };
+                Question thirdQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 3, QuestionStatement = "What is the status of task?", Type = 2 };
                 Question fourthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 4, QuestionStatement = "List Comment/Roadblock (if any).", Type = 2 };
-                Question fifthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 5, QuestionStatement = "Your task mail is ready. Are you willing to send it?", Type = 2 };
-                Question sixthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 6, QuestionStatement = "Are you sure you want to send email? After sending the email you won't be able to add any task for today.", Type = 2 };
+                Question fifthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 5, QuestionStatement = "Your task mail is ready. Do you want to send it?", Type = 2 };
+                Question sixthQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 6, QuestionStatement = "After sending the email you won't be able to add any task for today. Are you sure you want to send email?", Type = 2 };
                 Question seventhQuestionTaskMail = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 7, QuestionStatement = "Task Mail Complete", Type = 2 };
                 Question firstQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 1, QuestionStatement = "What did you do yesterday?", Type = 1 };
                 Question secondQuestionScrumBot = new Question() { CreatedOn = DateTime.UtcNow, OrderNumber = 2, QuestionStatement = "What are you going to do today?", Type = 1 };
