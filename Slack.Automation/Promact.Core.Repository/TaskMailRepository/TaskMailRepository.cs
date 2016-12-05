50bd9a989a394e54b86e3e07c11d5c007a0c15f1
Ronak <ronak.shah@promactinfo.com>
2016-12-05 15:27:42 +0530

    Improve naming conventions and routes. Fixes #105

--- Slack.Automation/Promact.Core.Repository/TaskMailRepository/TaskMailRepository.cs
+++ Slack.Automation/Promact.Core.Repository/TaskMailRepository/TaskMailRepository.cs
@@ -483,7 +483,7 @@ namespace Promact.Core.Repository.TaskMailRepository
             else if (role == _stringConstant.RoleTeamLeader)
             {
 
-                var user = _user.FirstOrDefault(x => x.Id == LoginId);
+                var user = _user.FirstOrDefault(x => x.Id == loginId);
                 var accessToken = await _attachmentRepository.AccessToken(user.UserName);
                 List<UserRoleAc> userRoles = await _oauthCallsRepository.GetListOfEmployeeAsync(user.Id, accessToken);
                 foreach (var userRole in userRoles)
@@ -590,7 +590,7 @@ namespace Promact.Core.Repository.TaskMailRepository
             { taskMailReportAcList = await TaskMailDetailsForSelectedDateAsync(id, name, role, createdOn, loginId, selectedDate); }
             else if (role == _stringConstant.RoleTeamLeader)
             {
-                var user = _user.FirstOrDefault(x => x.Id == LoginId);
+                var user = _user.FirstOrDefault(x => x.Id == loginId);
                 var accessToken = await _attachmentRepository.AccessToken(user.UserName);
                 List<UserRoleAc> userRolesAcList = await _oauthCallsRepository.GetListOfEmployeeAsync(user.Id, accessToken);
                 foreach (var userRoleAc in userRolesAcList)
@@ -704,7 +704,7 @@ namespace Promact.Core.Repository.TaskMailRepository
             }
             else if (role == _stringConstant.RoleTeamLeader)
             {
-                var user = _user.FirstOrDefault(x => x.Id == LoginId);
+                var user = _user.FirstOrDefault(x => x.Id == loginId);
                 var accessToken = await _attachmentRepository.AccessToken(user.UserName);
                 List<UserRoleAc> usersRole = await _oauthCallsRepository.GetListOfEmployeeAsync(user.Id, accessToken);
                 foreach (var userRole in usersRole)
