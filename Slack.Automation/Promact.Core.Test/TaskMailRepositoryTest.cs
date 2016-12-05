0d7fc60bb3fe1e241f579d6ffb2fc2eadb8a983b
Ronak <ronak.shah@promactinfo.com>
2016-12-05 12:27:54 +0530

    update naming conventions and stream line code in Task Mail Module and Fixes #105

--- Slack.Automation/Promact.Core.Test/TaskMailRepositoryTest.cs
+++ Slack.Automation/Promact.Core.Test/TaskMailRepositoryTest.cs
@@ -734,7 +734,7 @@ namespace Promact.Core.Test
         /// get the user information.
         /// </summary>
         [Fact, Trait("Category", "Required")]
-        public async void GetAllEmployeeAsync()
+        public async Task GetAllEmployeeAsync()
         {
 
             UserLoginInfo info = new UserLoginInfo(_stringConstant.PromactStringName, _stringConstant.AccessTokenForTest);
@@ -890,7 +890,7 @@ namespace Promact.Core.Test
         /// this test case for the task mail details for the selected date.
         /// </summary>
         [Fact, Trait("Category", "Required")]
-        public async void TaskMailDetailsReportSelectedDateAsync()
+        public async Task TaskMailDetailsReportSelectedDateAsync()
         {
             var userResponse = Task.FromResult(_stringConstant.UserDetailsFromOauthServer);
             var userRequestUrl = string.Format(_stringConstant.FirstAndSecondIndexStringFormat, _stringConstant.UserDetailsUrl, _stringConstant.FirstNameForTest);
@@ -921,7 +921,7 @@ namespace Promact.Core.Test
         /// this test case for the task mail details for the next date.
         /// </summary>
         [Fact, Trait("Category", "Required")]
-        public async void TaskMailDetailsReportNextPreviousDateAsync()
+        public async Task TaskMailDetailsReportNextPreviousDateAsync()
         {
             var userResponse = Task.FromResult(_stringConstant.UserDetailsFromOauthServer);
             var userRequestUrl = string.Format(_stringConstant.FirstAndSecondIndexStringFormat, _stringConstant.UserDetailsUrl, _stringConstant.FirstNameForTest);
