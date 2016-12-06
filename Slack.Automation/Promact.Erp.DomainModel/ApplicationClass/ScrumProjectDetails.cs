9be3c43cdd098823a6fc3849e94d284591cd0246
Gourav Agarwal <gourav@promactinfo.com>
2016-12-06 18:51:24 +0530

    removed unused code and converted properties to nullable type

--- Slack.Automation/Promact.Erp.DomainModel/ApplicationClass/ScrumProjectDetails.cs
+++ Slack.Automation/Promact.Erp.DomainModel/ApplicationClass/ScrumProjectDetails.cs
@@ -1,9 +1,4 @@
-﻿using Promact.Erp.DomainModel.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace Promact.Erp.DomainModel.ApplicationClass
 {
