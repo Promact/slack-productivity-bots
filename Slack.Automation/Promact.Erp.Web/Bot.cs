6f4dfd5a72b48e25e120d392759ae4c65177cf42
julie <julie@PR2C4R3Y2015>
2016-12-06 15:46:27 +0530

    commented code removed.Fixes#120

--- Slack.Automation/Promact.Erp.Web/Bot.cs
+++ Slack.Automation/Promact.Erp.Web/Bot.cs
@@ -93,8 +93,7 @@ namespace Promact.Erp.Web
         {
             _logger = container.Resolve<ILogger>();
             _stringConstant = container.Resolve<IStringConstantRepository>();
-            //try
-            //{
+
             _environmentVariableRepository = container.Resolve<IEnvironmentVariableRepository>();
             string botToken = _environmentVariableRepository.ScrumBotToken;
             SlackSocketClient client = new SlackSocketClient(botToken);//scrumBot
@@ -132,28 +131,7 @@ namespace Promact.Erp.Web
                     client.CloseSocket();
                     throw ex;
                 }
-                //catch (TaskCanceledException ex)
-                //{
-                //    client.SendMessage(showMethod, message.channel, _stringConstant.ErrorMsg);
-                //    _logger.Error("\n" + _stringConstant.LoggerScrumBot + " " + ex.InnerException + "\n" + ex.StackTrace);
-                //    client.CloseSocket();
-                //    throw ex;
-                //}
-                //catch (NullReferenceException ex)
-                //{
-                //    client.SendMessage(showMethod, message.channel, _stringConstant.ErrorMsg);
-                //    _logger.Error("\n" + _stringConstant.LoggerScrumBot + " " + ex.InnerException + "\n" + ex.StackTrace);
-                //    client.CloseSocket();
-                //    throw ex;
-                //}
             };
-
-            //}
-            //catch (Exception ex)
-            //{
-            //    _logger.Error("\n" + _stringConstant.LoggerScrumBot + " " + ex.Message + "\n" + ex.StackTrace);
-            //    throw ex;
-            //}
         }
 
     }
