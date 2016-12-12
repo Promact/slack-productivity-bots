4e4a15413dcb2f89f524b8c7754fa23d16151bc5
Siddhartha Shaw <siddhartha@promactinfo.com>
2016-12-12 13:06:45 +0530

    IncomingWebHook.cs - Updated UserId and IncomingWebHookUrl, from null to not null field

--- Slack.Automation/Promact.Erp.DomainModel/Models/IncomingWebHook.cs
+++ Slack.Automation/Promact.Erp.DomainModel/Models/IncomingWebHook.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,9 @@ namespace Promact.Erp.DomainModel.Models
     public class IncomingWebHook
     {
         public int Id { get; set; }
+        [Required]
         public string UserId { get; set; }
+        [Required]
         public string IncomingWebHookUrl { get; set; }
     }
 }
