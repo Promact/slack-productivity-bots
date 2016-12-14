10abfc63e2af53717f861d1d98fe49e47c6dc8c8
Ronak <ronak.shah@promactinfo.com>
2016-12-14 12:35:40 +0530

    rebase commit and Fixes #105

--- Slack.Automation/Promact.Core.Test/TaskMailRepositoryTest.cs
+++ Slack.Automation/Promact.Core.Test/TaskMailRepositoryTest.cs
@@ -858,18 +858,6 @@ namespace Promact.Core.Test
             Assert.Equal(1, result.Count);
         }
         
-            UserLoginInfo info = new UserLoginInfo(_stringConstant.PromactStringName, _stringConstant.AccessTokenForTest);
-            await _userManager.CreateAsync(user);
-            await _userManager.AddLoginAsync(user.Id, info);
-
-            var response = Task.FromResult(_stringConstant.EmployeeInformation);
-            var requestUrl = string.Format("{0}{1}", user.Id, _stringConstant.UserRoleUrl);
-            _mockHttpClient.Setup(x => x.GetAsync(_stringConstant.UserUrl, requestUrl, _stringConstant.AccessTokenForTest)).Returns(response);
-
-            var result = await _taskMailRepository.GetAllEmployeeAsync(user.Id);
-            Assert.Equal(1, result.Count);
-        }
-        
         /// <summary>
         /// this test case for the task mail details for the selected date.
         /// </summary>
