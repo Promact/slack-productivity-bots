5c0300a5d086279655ed35c05b61aae381cee1a1
Gourav Agarwal <gourav@promactinfo.com>
2016-12-21 18:17:19 +0530

    handled forbidden user in scrum and leave report module

--- Slack.Automation/Promact.Erp.Core/Controllers/ScrumReportController.cs
+++ Slack.Automation/Promact.Erp.Core/Controllers/ScrumReportController.cs
@@ -1,12 +1,17 @@
-﻿using Promact.Core.Repository.AttachmentRepository;
+﻿using Autofac.Extras.NLog;
+using Promact.Core.Repository.AttachmentRepository;
 using Promact.Core.Repository.ScrumReportRepository;
 using Promact.Erp.DomainModel.Models;
+using Promact.Erp.Util.ExceptionHandler;
+using Promact.Erp.Util.StringConstants;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
+
 
 namespace Promact.Erp.Core.Controllers
 {
@@ -18,14 +23,19 @@ namespace Promact.Erp.Core.Controllers
         private readonly IScrumReportRepository _scrumReportRepository;
         private readonly IAttachmentRepository _attachmentRepository;
         private ApplicationUserManager _userManager;
+        private readonly ILogger _logger;
+        private readonly IStringConstantRepository _stringConstant;
+
         #endregion
 
         #region Constructor
-        public ScrumReportController(IScrumReportRepository scrumRepository, IAttachmentRepository attachmentRepository, ApplicationUserManager userManager)
+        public ScrumReportController(IScrumReportRepository scrumRepository, IAttachmentRepository attachmentRepository, ApplicationUserManager userManager, ILogger logger, IStringConstantRepository stringConstant)
         {
             _scrumReportRepository = scrumRepository;
             _attachmentRepository = attachmentRepository;
             _userManager = userManager;
+            _logger = logger;
+            _stringConstant = stringConstant;
         }
         #endregion
 
@@ -88,9 +98,18 @@ namespace Promact.Erp.Core.Controllers
         [Route("")]
         public async Task<IHttpActionResult> ScrumProjectListAsync()
         {
-            var accessToken = await _attachmentRepository.UserAccessTokenAsync(User.Identity.Name);
-            var loginUser = await _userManager.FindByNameAsync(User.Identity.Name);
-            return Ok(await _scrumReportRepository.GetProjectsAsync(loginUser.Id, accessToken));
+            try
+            {
+                var accessToken = await _attachmentRepository.UserAccessTokenAsync(User.Identity.Name);
+                var loginUser = await _userManager.FindByNameAsync(User.Identity.Name);
+                return Ok(await _scrumReportRepository.GetProjectsAsync(loginUser.Id, accessToken));
+            }
+            catch (ForbiddenUserException)
+            {
+                _logger.Error(_stringConstant.ForbiddenUser);
+                return StatusCode(HttpStatusCode.Forbidden);
+            }
+
         }
 
         /**
@@ -125,11 +144,20 @@ namespace Promact.Erp.Core.Controllers
         [Route("{id}/detail")]
         public async Task<IHttpActionResult> ScrumDetailsAsync(int id)
         {
-            string queryString = Request.RequestUri.Query.Substring(1,24);
-            DateTime date = Convert.ToDateTime(queryString); 
-            var accessToken = await _attachmentRepository.UserAccessTokenAsync(User.Identity.Name);
-            var loginUser = await _userManager.FindByNameAsync(User.Identity.Name);
-            return Ok(await _scrumReportRepository.ScrumReportDetailsAsync(id, date,loginUser.Id, accessToken));
+            try
+            {
+                string queryString = Request.RequestUri.Query.Substring(1, 24);
+                DateTime date = Convert.ToDateTime(queryString);
+                var accessToken = await _attachmentRepository.UserAccessTokenAsync(User.Identity.Name);
+                var loginUser = await _userManager.FindByNameAsync(User.Identity.Name);
+                return Ok(await _scrumReportRepository.ScrumReportDetailsAsync(id, date, loginUser.Id, accessToken));
+            }
+            catch (ForbiddenUserException)
+            {
+                _logger.Error(_stringConstant.ForbiddenUser);
+                return StatusCode(HttpStatusCode.Forbidden);
+            }
+
         }
         #endregion
     }
