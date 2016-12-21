55a9fd1f10a73be80216b7ed001ff241e9fa732b
Siddhartha Shaw <siddhartha@promactinfo.com>
2016-12-21 10:38:42 +0530

    StringConstantRepository.cs - Update statement for error message of leave slash command

--- Slack.Automation/Promact.Erp.Util/StringConstants/StringConstantRepository.cs
+++ Slack.Automation/Promact.Erp.Util/StringConstants/StringConstantRepository.cs
@@ -1356,7 +1356,7 @@ namespace Promact.Erp.Util.StringConstants
         {
             get
             {
-                return string.Format("Sorry you cannot use leave slash command. Either user are not in Promact OAuth or yet u haven't login in promact-slack server. Click here {0} to add", AppSettingUtil.PromactErpUrl);
+                return string.Format("Sorry you cannot use leave slash command. Either user is not in Promact OAuth or yet u haven't login in promact-slack server. Click here {0} to add", AppSettingUtil.PromactErpUrl);
             }
         }
         public string LeaveListCommandForTest
