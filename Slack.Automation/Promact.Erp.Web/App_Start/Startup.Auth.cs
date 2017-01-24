3f65319a080ea22bb420da69f592cc0086cd8dd8
ankit <ankit@PR1C2R4Y2015>
2017-01-24 16:30:43 +0530

    upate startup.cs and web.config

--- Slack.Automation/Promact.Erp.Web/App_Start/Startup.Auth.cs
+++ Slack.Automation/Promact.Erp.Web/App_Start/Startup.Auth.cs
@@ -6,6 +6,9 @@ using Microsoft.Owin.Security.Cookies;
 using Owin;
 using Promact.Erp.DomainModel.Context;
 using Promact.Erp.DomainModel.Models;
+using Microsoft.Owin.Security.OpenIdConnect;
+using Promact.Erp.Util;
+using System.IdentityModel.Tokens;
 
 namespace Promact.Erp.Web
 {
@@ -19,31 +22,59 @@ namespace Promact.Erp.Web
             app.CreatePerOwinContext<ApplicationUserManager>(ApplicationUserManager.Create);
             app.CreatePerOwinContext<ApplicationSignInManager>(ApplicationSignInManager.Create);
 
+
+            app.UseCookieAuthentication(new CookieAuthenticationOptions
+            {
+                AuthenticationType = "Cookies"
+            });
+
+           // JwtSecurityTokenHandler.DefaultInboundClaimTypeMap.Clear();
+            
+            app.UseOpenIdConnectAuthentication(new OpenIdConnectAuthenticationOptions
+            {
+                Authority = AppSettingUtil.OAuthUrl,
+                ClientId = "Z6UFWD97GNN3G6F",
+                ClientSecret = "9U13m73avmqZV74hGKvgKebf9Xq8P7",
+                RedirectUri = AppSettingUtil.PromactErpUrl,
+                ResponseType = "code id_token",
+                Scope = "openid",
+                SignInAsAuthenticationType = "Cookies",
+                AuthenticationType = "oidc",
+                //RequireHttpsMetadata = false,
+                //GetClaimsFromUserInfoEndpoint = true,
+                //SaveTokens = true,
+                //AutomaticAuthenticate = true,
+                //AutomaticChallenge = true,
+                PostLogoutRedirectUri = AppSettingUtil.PromactErpUrl,
+                UseTokenLifetime = true
+
+            });
+
             // Enable the application to use a cookie to store information for the signed in user
             // and to use a cookie to temporarily store information about a user logging in with a third party login provider
             // Configure the sign in cookie
-            app.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-                AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                LoginPath = new PathString("/Account/Login"),
-                Provider = new CookieAuthenticationProvider
-                {
-                    // Enables the application to validate the security stamp when the user logs in.
-                    // This is a security feature which is used when you change a password or add an external login to your account.  
-                    OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
-                        validateInterval: TimeSpan.FromMinutes(30),
-                        regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
-                }
-            });            
-            app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            //app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //{
+            //    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            //    LoginPath = new PathString("/Account/Login"),
+            //    Provider = new CookieAuthenticationProvider
+            //    {
+            //        // Enables the application to validate the security stamp when the user logs in.
+            //        // This is a security feature which is used when you change a password or add an external login to your account.  
+            //        OnValidateIdentity = SecurityStampValidator.OnValidateIdentity<ApplicationUserManager, ApplicationUser>(
+            //            validateInterval: TimeSpan.FromMinutes(30),
+            //            regenerateIdentity: (manager, user) => user.GenerateUserIdentityAsync(manager))
+            //    }
+            //});            
+            //app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-            // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-            app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
+            //// Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
+            //app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
 
-            // Enables the application to remember the second login verification factor such as phone or email.
-            // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-            // This is similar to the RememberMe option when you log in.
-            app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
+            //// Enables the application to remember the second login verification factor such as phone or email.
+            //// Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
+            //// This is similar to the RememberMe option when you log in.
+            //app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
         }
     }
 }
