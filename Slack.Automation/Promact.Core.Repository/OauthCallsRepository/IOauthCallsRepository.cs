24f7c264484d4bdfea3e9d1e2aedcba4db95cd42
Ronak <ronak.shah@promactinfo.com>
2017-02-02 16:21:20 +0530

    Updated task mail report flow. Fixes #137

--- Slack.Automation/Promact.Core.Repository/OauthCallsRepository/IOauthCallsRepository.cs
+++ Slack.Automation/Promact.Core.Repository/OauthCallsRepository/IOauthCallsRepository.cs
@@ -60,17 +60,6 @@ namespace Promact.Core.Repository.OauthCallsRepository
         /// <returns>List of user. List of object of UserRoleAc</returns>
         Task<List<UserRoleAc>> GetListOfEmployeeAsync(string userId, string accessToken);
 
-
-        /// <summary>
-        /// List of employee under this employee
-        /// </summary>
-        /// <param name="userId"></param>
-        /// <param name="accessToken"></param>
-        /// <returns>List of user</returns>
-        Task<List<UserRoleAc>> GetListOfEmployeeAsync(string userId);
-
-        
-
         /// <summary>
         /// This method is used to fetch list of users/employees of the given group name. - JJ
         /// </summary>
