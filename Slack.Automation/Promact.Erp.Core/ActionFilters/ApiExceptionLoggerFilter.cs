3b4b1ca2e5aab797ad122e983b0c596255c6f650
Ronak <ronak.shah@promactinfo.com>
2017-02-14 19:21:05 +0530

    added APIExceptionLoggerFilter.cs and updated Global.asax.cs #176

---
+++ Slack.Automation/Promact.Erp.Core/ActionFilters/ApiExceptionLoggerFilter.cs
@@ -0,0 +1,30 @@
+﻿using Autofac.Extras.NLog;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+
+namespace Promact.Erp.Core.ActionFilters
+{
+    public class ApiExceptionLoggerFilter : IExceptionFilter
+    {
+        private ILogger _logger;
+
+        public ApiExceptionLoggerFilter(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public bool AllowMultiple { get { return true; } }
+
+        public Task ExecuteExceptionFilterAsync(
+                HttpActionExecutedContext actionExecutedContext,
+                CancellationToken cancellationToken)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                _logger.Error("Web Service Error Message:" + actionExecutedContext.Exception.Message);
+                _logger.Error("Web Service Error StackTrace:" + actionExecutedContext.Exception.StackTrace);
+            }, cancellationToken);
+        }
+    }
+}
