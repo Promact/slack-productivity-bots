24c8b168c26b02060a362c5deccca49218c59048
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 13:03:35 +0530

    added mail setting configuration

---
+++ Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MailSetting } from './mailsetting.model';
+
+@Component({
+    templateUrl: './app/shared/MailSetting/mailsetting.html',
+})
+export class MailSettingComponent implements OnInit {
+    currentLocation: string;
+    constructor() { }
+    ngOnInit() {
+
+    }
+
+    getCurrentLocationAndProvideModule() {
+        this.currentLocation = window.location.hash;
+        let listofString = this.currentLocation.split('/');
+        return listofString[1];
+    }
+
+    addMailSetting(mailSetting: MailSetting) {
+        mailSetting.Module = this.getCurrentLocationAndProvideModule();
+    }
+}
