379e25f86ac630b29d79c85fb61aaf7b484fdce5
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 14:16:42 +0530

    added md2 package and updated mail setting

--- Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.component.ts
@@ -8,6 +8,7 @@ import { LoaderService } from '../../shared/loader.service';
 //import { Md2Toast } from 'md2';
 import { MailSettingAC } from './mailsettingAC.model';
 
+
 @Component({
     templateUrl: './app/shared/MailSetting/mailsetting.html',
 })
