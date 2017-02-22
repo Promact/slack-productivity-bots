c8df523928d5a187b9564f1392f633ce42297c22
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 09:08:54 +0530

    PromactErpContext.cs- added table for group, groupemailmapping, mailsetting and mailsettingmapping Group.cs - added new class for Group GroupEmailMapping.cs - added new class for GroupEmailMapping MailSetting.cs - added new class for MailSetting MailSettingMapping.cs - added new class for MailSettingMapping

---
+++ Slack.Automation/Promact.Erp.DomainModel/Models/Group.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Promact.Erp.DomainModel.Models
+{
+    public class Group
+    {
+        /// <summary>
+        /// Primary Key of Group table
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Name of Group
+        /// </summary>
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        /// <summary>
+        /// Type of Group
+        /// </summary>
+        public int Type { get; set; }
+        /// <summary>
+        /// Created Date of Group
+        /// </summary>
+        public DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// Updated Date of Group
+        /// </summary>
+        public DateTime? UpdatedDate { get; set; }
+
+        public ICollection<GroupEmailMapping> GroupEmailMapping { get; set; }
+    }
+}
