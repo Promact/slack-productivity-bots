f52c41edc794f3d4eb41ef9deee336e50480ddaf
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 13:03:35 +0530

    added mail setting configuration

--- Slack.Automation/Promact.Erp.Web/app/app.module.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.module.ts
@@ -15,10 +15,11 @@ import { TaskMailModule } from './taskmail/taskMail.module';
 import { LeaveModule } from './leaveReport/leaveReport.module';
 import { ScrumModule } from './ScrumReport/scrumReport.module';
 import { AppComponentService } from './appcomponent.service';
+import { MailSettingComponent } from './shared/mailsetting/mailsetting.component';
 
 
 @NgModule({
-    declarations: [AppComponent],
+    declarations: [AppComponent, MailSettingComponent],
     imports: [
         BrowserModule,
         HttpModule,
