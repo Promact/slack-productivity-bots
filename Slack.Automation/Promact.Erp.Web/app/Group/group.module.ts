b802aa2d9a7332602b9057f833d49a23a823dd7a
ankit <ankit@PR1C2R4Y2015>
2017-02-22 12:17:41 +0530

    implement Group Module

---
+++ Slack.Automation/Promact.Erp.Web/app/Group/group.module.ts
@@ -0,0 +1,22 @@
+﻿import { NgModule } from "@angular/core";
+import { SharedModule } from '../shared/shared.module';
+import { groupReportRoutes } from "./group.routes";
+import { GroupComponent } from "./group.component";
+import { GroupAddComponent } from "./GroupAdd/groupAdd.component";
+import { GroupEditComponent } from "./GroupAdd/groupEdit.component";
+
+@NgModule({
+    imports: [
+        groupReportRoutes,
+        SharedModule
+    ],
+    declarations: [
+        GroupComponent,
+        GroupAddComponent,
+        GroupEditComponent,
+
+
+    ],
+    providers: []
+})
+export class GroupModule { }
