b3c90131022bcac53c5dcdf51f0c4ffe241061ac
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 16:16:20 +0530

    added md-select for project drop down

--- Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.component.ts
@@ -2,29 +2,28 @@
 import { Router } from '@angular/router';
 import { MailSetting } from './mailsetting.model';
 import { NgModule } from "@angular/core";
-
+import { Project } from './project.model';
 
 @Component({
     templateUrl: './app/shared/MailSetting/mailsetting.html',
 })
 export class MailSettingComponent implements OnInit {
-    mailSetting: MailSetting;
-    projectList: Array<string>;
+    mailSettinglist: Array<Project>;
+    emailList: Array<string>;
     currentLocation: string;
-    SendMail: string;
     constructor() { }
     ngOnInit() {
-        this.SendMail = "test";
-        //this.projectList = [];
+        this.emailList = new Array<string>();
+        this.emailList = ["hello", "bye"];
     }
 
-    //getCurrentLocationAndProvideModule() {
-    //    this.currentLocation = window.location.hash;
-    //    let listofString = this.currentLocation.split('/');
-    //    return listofString[1];
-    //}
+    getCurrentLocationAndProvideModule() {
+        this.currentLocation = window.location.hash;
+        let listofString = this.currentLocation.split('/');
+        return listofString[1];
+    }
 
-    //addMailSetting(mailSetting: MailSetting) {
-    //    mailSetting.Module = this.getCurrentLocationAndProvideModule();
-    //}
+    addMailSetting(mailSetting: MailSetting) {
+        mailSetting.Module = this.getCurrentLocationAndProvideModule();
+    }
 }
