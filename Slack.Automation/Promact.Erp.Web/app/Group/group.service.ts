00647ab57082e94097198ff37c2632ea67f5af4a
ankit <ankit@PR1C2R4Y2015>
2017-02-22 12:17:41 +0530

    implement Group Module

---
+++ Slack.Automation/Promact.Erp.Web/app/Group/group.service.ts
@@ -0,0 +1,14 @@
+﻿import { Injectable } from '@angular/core';
+import { Http, Headers, RequestOptions, Response } from "@angular/http";
+import { Observable } from 'rxjs/Rx';
+import { StringConstant } from '../shared/stringConstant';
+
+@Injectable()
+export class GroupService {
+    constructor(private http: Http, private stringConstant: StringConstant) {
+
+
+
+    }
+}
+
