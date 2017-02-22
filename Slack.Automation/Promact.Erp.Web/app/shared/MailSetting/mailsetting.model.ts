1ad662cd9fbe17ac6da1fa02e1446d37ae7a194c
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 14:16:42 +0530

    added md2 package and updated mail setting

--- Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.model.ts
+++ Slack.Automation/Promact.Erp.Web/app/shared/MailSetting/mailsetting.model.ts
@@ -1,6 +1,8 @@
 ﻿export class MailSetting {
     Id: number;
     Module: string;
+    ProjectId: number;
+    ProjectName : string
     To: Array<string>;
     CC: Array<string>;
     SendMail: boolean;
