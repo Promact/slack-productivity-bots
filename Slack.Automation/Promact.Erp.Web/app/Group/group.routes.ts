a9ecda882c25871619acab786aee7d8e8fcaa637
ankit <ankit@PR1C2R4Y2015>
2017-02-22 12:17:41 +0530

    implement Group Module

---
+++ Slack.Automation/Promact.Erp.Web/app/Group/group.routes.ts
@@ -0,0 +1,9 @@
+﻿import { ModuleWithProviders } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+
+const GroupReportRoutes : Routes = [{
+    //path: "group",
+    //component: [],
+    //children: []
+}];
+export const groupReportRoutes: ModuleWithProviders = RouterModule.forChild(GroupReportRoutes);
