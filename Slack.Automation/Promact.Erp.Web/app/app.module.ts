50c3121df6af470712ada3dd86b630bf73406bb1
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-22 14:16:42 +0530

    added md2 package and updated mail setting

--- Slack.Automation/Promact.Erp.Web/app/app.module.ts
+++ Slack.Automation/Promact.Erp.Web/app/app.module.ts
@@ -16,6 +16,7 @@ import { LeaveModule } from './leaveReport/leaveReport.module';
 import { ScrumModule } from './ScrumReport/scrumReport.module';
 import { AppComponentService } from './appcomponent.service';
 import { MailSettingComponent } from './shared/mailsetting/mailsetting.component';
+import { Md2Module } from 'md2';
 import { EmailHashCode } from './shared/emailHashCode';
 
 @NgModule({
@@ -26,7 +27,8 @@ import { EmailHashCode } from './shared/emailHashCode';
         routing,
         TaskMailModule,
         LeaveModule,
-        ScrumModule
+        ScrumModule,
+        Md2Module.forRoot()
     ],
     bootstrap: [AppComponent],
     providers: [StringConstant, AppComponentService, LoaderService, { provide: LocationStrategy, useClass: HashLocationStrategy }, EmailHashCode]
