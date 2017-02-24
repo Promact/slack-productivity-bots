bd7132634c1ef8f617e918292da0190dfdee5de3
ankit <ankit@PR1C2R4Y2015>
2017-02-24 11:08:52 +0530

    update grouplist.component.ts and groupList.html

--- Slack.Automation/Promact.Erp.Web/app/Group/GroupList/groupList.component.ts
+++ Slack.Automation/Promact.Erp.Web/app/Group/GroupList/groupList.component.ts
@@ -3,12 +3,14 @@ import { Router } from "@angular/router";
 import { LoaderService } from '../../shared/loader.service';
 import { StringConstant } from '../../shared/stringConstant';
 import { GroupService } from '../group.service';
+import { GroupModel } from '../group.model';
 
 @Component({
     templateUrl: './app/Group/GroupList/GroupList.html',
     providers: [StringConstant]
 })
 export class GroupListComponent implements OnInit {
+    groupList: Array<GroupModel>;
     constructor(private router: Router, private stringConstant: StringConstant, private loader: LoaderService, private groupService: GroupService) {
 
     }
@@ -16,11 +18,16 @@ export class GroupListComponent implements OnInit {
     ngOnInit() {
         this.loader.loader = true;
         this.groupService.getListOfGroup().then((result) => {
-
+            this.groupList = result;
             this.loader.loader = false;
         }, err => {
 
         });
     }
+
+    addNewGroup() {
+        this.router.navigate(['/group/add']);
+    }
+
 }
 
