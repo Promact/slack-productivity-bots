8d22a0dee4eb0c44a21eaa2202f390bdeb542e36
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-24 18:34:58 +0530

    commit to resolve the conflict

---
+++ Slack.Automation/Promact.Erp.Web/app/shared/emailHashCode.ts
@@ -0,0 +1,7 @@
+﻿declare var emailHashCode: { EmailHashCode: string, };
+export class EmailHashCode {
+    public hashCode: string;
+    constructor() {
+        this.hashCode = emailHashCode.EmailHashCode;
+    }
+}
