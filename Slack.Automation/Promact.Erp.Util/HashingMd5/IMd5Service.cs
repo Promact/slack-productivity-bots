c0302824867dc92aaa795b3040be326d2986e1db
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-24 18:34:58 +0530

    commit to resolve the conflict

---
+++ Slack.Automation/Promact.Erp.Util/HashingMd5/IMd5Service.cs
@@ -0,0 +1,13 @@
+﻿namespace Promact.Erp.Util.HashingMd5
+{
+    public interface IMd5Service
+    {
+        /// <summary>
+        /// take any string and encrypt it using MD5 then
+        /// return the encrypted data 
+        /// </summary>
+        /// <param name="data">input text you will enterd to encrypt it</param>
+        /// <returns>return the encrypted text as hexadecimal string</returns>
+        string GetMD5HashData(string data);
+    }
+}
