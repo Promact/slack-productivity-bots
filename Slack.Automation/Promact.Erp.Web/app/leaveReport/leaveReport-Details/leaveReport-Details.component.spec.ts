e1b22373100252fed2894cc3c8e718c60c0ad3ce
julie <julie@PR2C4R3Y2015>
2017-02-27 15:49:47 +0530

    updated leave report test cases. Fixes#204

--- Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-Details/leaveReport-Details.component.spec.ts
+++ Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-Details/leaveReport-Details.component.spec.ts
@@ -106,7 +106,6 @@ describe('LeaveReport Detials Tests', () => {
         expect(leaveReportDetailsComponent.exportDataToPdf).toHaveBeenCalled();
     });
 
-});
 
 
 class MockLeaveReport extends LeaveReport {
