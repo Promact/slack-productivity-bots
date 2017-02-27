81d5d43830c3829796276a3b04d1586f9480797b
julie <julie@PR2C4R3Y2015>
2017-02-27 15:49:47 +0530

    updated leave report test cases. Fixes#204

--- Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-Details/leaveReport-Details.component.spec.ts
+++ Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-Details/leaveReport-Details.component.spec.ts
@@ -1,56 +1,89 @@
-﻿//declare let describe, it, beforeEach, expect;
-//import { async, inject, TestBed, ComponentFixture, tick, fakeAsync } from '@angular/core/testing';
-//import { Provider } from "@angular/core";
-//import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
-//import { Observable } from 'rxjs/Observable';
-//import { RouterLinkStubDirective } from '../../shared/mock/mock.routerLink';
-//import { LeaveModule } from '../../leaveReport/leaveReport.module';
-//import { LeaveReportService } from '../leaveReport.service';
-//import { MockLeaveReportService } from '../../shared/mock/mock.leaveReport.service';
-//import { StringConstant } from '../../shared/stringConstant';
-//import { LeaveReportDetailsComponent } from './leaveReport-Details.component';
-//import { LoaderService } from '../../shared/loader.service';
-//import { Md2SelectChange } from 'md2';
-//import { AppModule } from '../../app.module';
+﻿declare let describe, it, beforeEach, expect;
+import { async, inject, TestBed, ComponentFixture } from '@angular/core/testing';
+import { Provider } from "@angular/core";
+import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { RouterLinkStubDirective } from '../../shared/mock/mock.routerLink';
+import { LeaveModule } from '../../leaveReport/leaveReport.module';
+import { LeaveReportService } from '../leaveReport.service';
+import { MockLeaveReportService } from '../../shared/mock/mock.leaveReport.service';
+import { StringConstant } from '../../shared/stringConstant';
+import { LeaveReportDetailsComponent } from './leaveReport-Details.component';
+import { LoaderService } from '../../shared/loader.service';
 
-//let promise: TestBed;
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { LeaveReport } from '../../leaveReport/leaveReport-List/leaveReport-List.model';
+import { LeaveReportDetail } from '../../leaveReport/leaveReport-Details/leaveReport-Details.model';
 
-//describe('LeaveReport Detials Tests', () => {
-//    const routes: Routes = [];
-//    class MockLoaderService { }
-//    class MockMd2Select { }
-//    class MockActivatedRoute extends ActivatedRoute {
-//        constructor() {
-//            super();
-//            this.params = Observable.of({ id: "1" });
-//        }
-//    };
+let promise: TestBed;
 
-//    beforeEach(async(() => {
-//        this.promise = TestBed.configureTestingModule({
-//            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
-//            imports: [AppModule, RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
-//            ],
-//            providers: [
-//                { provide: ActivatedRoute, useClass: MockActivatedRoute },
-//                { provide: LeaveReportService, useClass: MockLeaveReportService },
-//                { provide: StringConstant, useClass: StringConstant },
-//                { provide: LoaderService, useClass: MockLoaderService },
-//                { provide: Md2SelectChange, useClass: MockMd2Select },
-//            ]
-//        }).compileComponents();
-//    }));
+describe('LeaveReport Detials Tests', () => {
+    const routes: Routes = [];
+    class MockLoaderService { }
+    class MockActivatedRoute extends ActivatedRoute {
+        constructor() {
+            super();
+            this.params = Observable.of({ id: "1" });
+        }
+    };
 
-//    it('Shows details of leave report for an employee on initialization', fakeAsync(() => {
-//        let fixture = TestBed.createComponent(LeaveReportDetailsComponent); //Create instance of component            
-//        let leaveReportDetailsComponent = fixture.componentInstance;
-//        let result = leaveReportDetailsComponent.ngOnInit();
-//        tick();
-//        expect(leaveReportDetailsComponent.leaveReportDetail.length).toBe(1);
-//    }));
-//});
+    beforeEach(async(() => {
+        this.promise = TestBed.configureTestingModule({
+            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
+            imports: [LeaveModule, RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
+            ],
+            providers: [
+                { provide: ActivatedRoute, useClass: MockActivatedRoute },
+                { provide: LeaveReportService, useClass: MockLeaveReportService },
+                { provide: StringConstant, useClass: StringConstant },
+                { provide: LoaderService, useClass: MockLoaderService }
+            ]
+        }).compileComponents();
+    }));
 
 
+    it('Shows details of leave report for an employee on initialization but there are no leave reports', () => {
+        let mockLeaveReportDetails = new Array<MockLeaveReportDetails>();
+        let fixture = TestBed.createComponent(LeaveReportDetailsComponent); //Create instance of component            
+        let leaveReportDetailsComponent = fixture.componentInstance;
+        let leaveReportService = fixture.debugElement.injector.get(LeaveReportService);
+        spyOn(leaveReportService, "getLeaveReportDetail").and.returnValue(new BehaviorSubject(mockLeaveReportDetails).asObservable());
+        let result = leaveReportDetailsComponent.getLeaveReportDetail();
+        expect(leaveReportDetailsComponent.leaveReportDetail.length).toBe(0);
+    });
+
+
+    it('Shows details of leave report for an employee on initialization', () => {
+        let mockLeaveReportDetails = new Array<MockLeaveReportDetails>();
+        let mockLeaveReportDetail = new MockLeaveReportDetails();
+        mockLeaveReportDetail.EmployeeUserName = "abc@abc.com";
+        mockLeaveReportDetail.EmployeeName = "abc";
+        mockLeaveReportDetail.LeaveFrom = "1/1/16";
+        mockLeaveReportDetails.push(mockLeaveReportDetail);
+
+        let fixture = TestBed.createComponent(LeaveReportDetailsComponent); //Create instance of component            
+        let leaveReportDetailsComponent = fixture.componentInstance;
+        let leaveReportService = fixture.debugElement.injector.get(LeaveReportService);
+        spyOn(leaveReportService, "getLeaveReportDetail").and.returnValue(new BehaviorSubject(mockLeaveReportDetails).asObservable());
+        let result = leaveReportDetailsComponent.getLeaveReportDetail();
+        expect(leaveReportDetailsComponent.leaveReportDetail.length).toBe(1);
+    });
+
+});
+
+
+class MockLeaveReport extends LeaveReport {
+    constructor() {
+        super();
+    }
+}
+
+class MockLeaveReportDetails extends LeaveReportDetail {
+    constructor() {
+        super();
+    }
+}
 
 
 
--- Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-List/leaveReport-List.component.spec.ts
+++ Slack.Automation/Promact.Erp.Web/app/leaveReport/leaveReport-List/leaveReport-List.component.spec.ts
@@ -1,17 +1,15 @@
-﻿//declare let describe, it, beforeEach, expect;
-//import { async, inject, TestBed, ComponentFixture, tick, fakeAsync } from '@angular/core/testing';
-//import { Provider } from "@angular/core";
-//import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
-//import { Observable } from 'rxjs/Observable';
-//import { RouterLinkStubDirective } from '../../shared/mock/mock.routerLink';
-//import { LeaveModule } from '../../leaveReport/leaveReport.module';
-//import { LeaveReportService } from '../leaveReport.service';
-//import { MockLeaveReportService } from '../../shared/mock/mock.leaveReport.service';
-//import { StringConstant } from '../../shared/stringConstant';
-//import { LeaveReportListComponent } from './leaveReport-List.component';
-//import { LoaderService } from '../../shared/loader.service';
-//import { Md2SelectChange } from 'md2';
-//import { AppModule } from '../../app.module';
+﻿declare let describe, it, beforeEach, expect;
+import { async, inject, TestBed, ComponentFixture } from '@angular/core/testing';
+import { Provider } from "@angular/core";
+import { Router, ActivatedRoute, RouterModule, Routes } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { RouterLinkStubDirective } from '../../shared/mock/mock.routerLink';
+import { LeaveModule } from '../../leaveReport/leaveReport.module';
+import { LeaveReportService } from '../leaveReport.service';
+import { MockLeaveReportService } from '../../shared/mock/mock.leaveReport.service';
+import { StringConstant } from '../../shared/stringConstant';
+import { LeaveReportListComponent } from './leaveReport-List.component';
+import { LoaderService } from '../../shared/loader.service';
 
 //let promise: TestBed;
 
@@ -22,20 +20,31 @@
 //    class MockMd2Select { }
 //    const routes: Routes = [];
 
-//    beforeEach(async(() => {
-//        this.promise = TestBed.configureTestingModule({
-//            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
-//            imports: [AppModule, RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
-//            ],
-//            providers: [
-//                { provide: Router, useClass: MockRouter },
-//                { provide: LeaveReportService, useClass: MockLeaveReportService },
-//                { provide: StringConstant, useClass: StringConstant },
-//                { provide: LoaderService, useClass: MockLoaderService },
-//                { provide: Md2SelectChange, useClass: MockMd2Select }
-//            ]
-//        }).compileComponents();
-//    }));
+    const routes: Routes = [];
+
+    beforeEach(async(() => {
+        this.promise = TestBed.configureTestingModule({
+            declarations: [RouterLinkStubDirective], //Declaration of mock routerLink used on page.
+            imports: [LeaveModule, RouterModule.forRoot(routes, { useHash: true }) //Set LocationStrategy for component. 
+            ],
+            providers: [
+                { provide: Router, useClass: MockRouter },
+                { provide: LeaveReportService, useClass: MockLeaveReportService },
+                { provide: StringConstant, useClass: StringConstant },
+                { provide: LoaderService, useClass: MockLoaderService }
+            ]
+        }).compileComponents();
+    }));
+
+    it('Shows list of leaveReports on initialization', () => done => {
+        this.promise.then(() => {
+            let fixture = TestBed.createComponent(LeaveReportListComponent); //Create instance of component            
+            let leaveReportListComponent = fixture.componentInstance;
+            let result = leaveReportListComponent.ngOnInit();
+            expect(leaveReportListComponent.leaveReports.length).toBe(1);
+            done();
+        });
+    });
 
 //    it('Shows list of leaveReports on initialization', fakeAsync(() => {
 //        let fixture = TestBed.createComponent(LeaveReportListComponent); //Create instance of component            
@@ -45,19 +54,31 @@
 //        expect(leaveReportListComponent.leaveReports.length).toBe(1);
 //    }));
 
+    it('Downloads report of leave reports on export to pdf', () => done => {
+        this.promise.then(() => {
+            let fixture = TestBed.createComponent(LeaveReportListComponent); //Create instance of component            
+            let leaveReportListComponent = fixture.componentInstance;
+            leaveReportListComponent.exportDataToPdf();
+            console.log(leaveReportListComponent.leaveReports.push());
+            expect(leaveReportListComponent.exportDataToPdf).toBe();
+        });
+    });
 
-//it('Downloads report of leave reports on export to pdf', fakeAsync(() => {
-//        let fixture = TestBed.createComponent(LeaveReportListComponent); //Create instance of component            
-//        let leaveReportListComponent = fixture.componentInstance;
-//        leaveReportListComponent.exportDataToPdf();
-//        console.log(leaveReportListComponent.leaveReports.push());
-//        tick();
-//        expect(leaveReportListComponent.exportDataToPdf).toBe();
-//    }));
-//});
+});
 
 
 
+class MockLeaveReport extends LeaveReport {
+    constructor() {
+        super();
+    }
+}
+
+class MockLeaveReportDetails extends LeaveReportDetail {
+    constructor() {
+        super();
+    }
+}
 
 
 
