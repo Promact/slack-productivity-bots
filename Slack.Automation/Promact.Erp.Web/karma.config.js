5e0f7fcfec0eeeae93885aae1d87951ac335427f
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-27 15:08:41 +0530

    updated karma.config.js
    updated karma.config.js

--- Slack.Automation/Promact.Erp.Web/karma.config.js
+++ Slack.Automation/Promact.Erp.Web/karma.config.js
@@ -70,7 +70,7 @@ module.exports = function (config) {
         // test results reporter to use
         // possible values: 'dots', 'progress'
         // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-        reporters: ['progress', 'coverage', 'coveralls'],
+        reporters: ['progress', 'coverage'],
 
         coverageReporter: {
             reporters: [
@@ -119,8 +119,7 @@ module.exports = function (config) {
         plugins: [
             'karma-jasmine',
             'karma-chrome-launcher',
-            'karma-coverage',
-            'karma-coveralls'
+            'karma-coverage'
         ],
 
     })
