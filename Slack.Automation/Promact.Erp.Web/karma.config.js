06f4d291fa7639d703df054344832849bff49658
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-02-28 12:34:36 +0530

    commit to rebuild

--- Slack.Automation/Promact.Erp.Web/karma.config.js
+++ Slack.Automation/Promact.Erp.Web/karma.config.js
@@ -63,7 +63,7 @@ module.exports = function (config) {
 
         // preprocess matching files before serving them to the browser
         // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-        preprocessors: {
+        preprocessors: { 
             '**/app/*/*component.js': ['coverage'],
             '**/app/**/*component.js': ['coverage']
         },
