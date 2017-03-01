d0aed3c8ff810e201ac2ac38750e4326038cc607
Ronak <ronak.shah@promactinfo.com>
2017-03-01 09:49:15 +0530

    Updated test cases in Angular 2.4.8 and Fixes #208 , #217,#206

--- Slack.Automation/Promact.Erp.Web/app/shared/mock/mock.scrumReport.service.ts
+++ Slack.Automation/Promact.Erp.Web/app/shared/mock/mock.scrumReport.service.ts
@@ -5,65 +5,55 @@ import { ScrumDetails } from '../../ScrumReport/scrumProject-Details/scrumProjec
 import { EmployeeScrumAnswers } from '../../ScrumReport/scrumProject-Details/scrumProject-EmployeeScrumDetails.model';
 import { ScrumProject } from '../../ScrumReport/scrumProject-List/scrumProject-List.model';
 import { StringConstant } from '../stringConstant';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+
 
 @Injectable()
 
 export class MockScrumReportService {
 
-    constructor(private connection: TestConnection, private stringConstant: StringConstant) { }
+    constructor(private stringConstant: StringConstant) { }
 
     getScrumProjects() {
         let mockScrumProjects = new Array<ScrumProject>();
         let mockScrumProject = new ScrumProject();
-        mockScrumProject.Name = "aaaa";
+        mockScrumProject.Name = this.stringConstant.projectName;
         mockScrumProjects.push(mockScrumProject);
-        let connection = this.getMockResponse(this.stringConstant.scrum, mockScrumProjects);
-        return connection;
-    }    
+        return new BehaviorSubject(mockScrumProjects).asObservable()
+    }
 
     getScrumDetails(id: number, date: string) {
         let mockScrumDetails = new ScrumDetails();
         let mockEmployeeScrumAnswers = new Array<EmployeeScrumAnswers>();
         let mockEmployeeScrumAnswer = new EmployeeScrumAnswers();
-        mockEmployeeScrumAnswer.Answer1 = "abc";
-        mockEmployeeScrumAnswer.Answer2 = "abc2";
-        mockEmployeeScrumAnswer.Answer3 = "no";
-        mockEmployeeScrumAnswer.EmployeeName = "xyz";
+        mockEmployeeScrumAnswer.Answer1 = this.stringConstant.Answer1;
+        mockEmployeeScrumAnswer.Answer2 = this.stringConstant.Answer2;
+        mockEmployeeScrumAnswer.Answer3 = this.stringConstant.Answer3;
+        mockEmployeeScrumAnswer.EmployeeName = this.stringConstant.EmployeeName;
         mockEmployeeScrumAnswers.push(mockEmployeeScrumAnswer);
         if (id === 123) {
             mockScrumDetails.ScrumDate = date;
-            mockScrumDetails.ProjectCreationDate = "1/1/16";
+            mockScrumDetails.ProjectCreationDate = this.stringConstant.ProjectCreationDate;
             mockScrumDetails.EmployeeScrumAnswers = mockEmployeeScrumAnswers;
         }
-
-        let connection = this.getMockResponse(this.stringConstant.scrum + this.stringConstant.slash + id + this.stringConstant.detail, mockScrumDetails);
-        return connection;
-    }
-
-    getMockResponse(api: string, mockBody: Array<ScrumProject> | ScrumDetails) {
-        let connection = this.connection.mockConnection(api);
-        let response = new Response(new ResponseOptions({ body: mockBody }));
-
-        connection.mockRespond(response.json());
-        return connection.response;
+        return new BehaviorSubject(mockScrumDetails).asObservable()
     }
 }
 
-    class MockEmployeeScrumAnswers extends EmployeeScrumAnswers {
-        constructor() {
-            super();
-        }
+class MockEmployeeScrumAnswers extends EmployeeScrumAnswers {
+    constructor() {
+        super();
     }
+}
 
-    class MockScrumDetails extends ScrumDetails {
-        constructor() {
-            super();
-        }
+class MockScrumDetails extends ScrumDetails {
+    constructor() {
+        super();
     }
+}
 
-    class MockScrumProject extends ScrumProject {
-        constructor() {
-            super();
-        }
-    } 
-
+class MockScrumProject extends ScrumProject {
+    constructor() {
+        super();
+    }
+} 
--- Slack.Automation/Promact.Erp.Web/app/shared/stringConstant.ts
+++ Slack.Automation/Promact.Erp.Web/app/shared/stringConstant.ts
@@ -59,5 +59,11 @@ export class StringConstant {
     updated = "updated";
     project = "project";
     group = "group";
+    Answer1 = "abc";
+    Answer2 = "abc2";
+    Answer3 = "no";
+    EmployeeName = "xyz";
+    ProjectCreationDate = "1/1/16";
+    projectName = "aaaa";
     module = "module";
 }
