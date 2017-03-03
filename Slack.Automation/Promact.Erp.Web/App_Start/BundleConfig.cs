cb1c8f186b94a294d7ee073b672a6503d3153e08
asra <asra@ASRAKHAN>
2017-03-03 14:17:33 +0530

    commit revert

--- Slack.Automation/Promact.Erp.Web/App_Start/BundleConfig.cs
+++ Slack.Automation/Promact.Erp.Web/App_Start/BundleConfig.cs
@@ -24,18 +24,7 @@ namespace Promact.Erp.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
-                      "~/Content/site.css",
-                       "~/Content/css/erp-custom.css",
-                       "~/Content/js/theme/bootstrap-daterangepicker/daterangepicker.css"
-                      ));
-
-            bundles.Add(new ScriptBundle("~/bundles/themejs").Include(
-                     "~/Content/js/icheck.js",
-                     "~/Content/js/switchery.js",
-                "~/Content/js/theme/bootstrap-daterangepicker/daterangepicker.js",
-                "~/Content/js/theme/custom.js"
-
-               ));
+                      "~/Content/site.css"));
         }
     }
 }
