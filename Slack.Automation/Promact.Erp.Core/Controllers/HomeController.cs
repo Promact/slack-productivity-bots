b5619eeef86c95bcbb07b508aac6b6b347c501a8
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-03-17 17:48:40 +0530

    HomeController.cs - updated the route

--- Slack.Automation/Promact.Erp.Core/Controllers/HomeController.cs
+++ Slack.Automation/Promact.Erp.Core/Controllers/HomeController.cs
@@ -221,8 +221,8 @@ namespace Promact.Erp.Core.Controllers
         *     "Description":"Add to slack button will redirect here and it will open a Slack OAuth Authorization Page for our app"
         * }
         */
-        [Route("Home/SlackOAuthAuthorization/{id}")]
-        public ActionResult SlackOAuthAuthorization(int id)
+        [Route("Home/SlackOAuthAuthorization/{configurationId}")]
+        public ActionResult SlackOAuthAuthorization(int configurationId)
         {
             try
             {
