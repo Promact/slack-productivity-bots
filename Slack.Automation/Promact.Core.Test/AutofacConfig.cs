46fd4a38e34df3f3ee55841c11e62c1cab6adc72
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-03-17 13:57:48 +0530

    AutofacConfig.cs - register ConfigurationRepository ConfigurationRepositoryTest.cs - added test case for ConfigurationRepository Configuration.cs - added seed data for Configuration

--- Slack.Automation/Promact.Core.Test/AutofacConfig.cs
+++ Slack.Automation/Promact.Core.Test/AutofacConfig.cs
@@ -36,6 +36,7 @@ using Promact.Core.Repository.MailSettingRepository;
 using Promact.Core.Repository.MailSettingDetailsByProjectAndModule;
 using Promact.Core.Repository.ScrumSetUpRepository;
 using Promact.Core.Repository.GroupRepository;
+using Promact.Core.Repository.ConfigurationRepository;
 using Promact.Core.Repository.RedmineRepository;
 
 namespace Promact.Core.Test
@@ -99,6 +100,7 @@ namespace Promact.Core.Test
             var httpContextObject = httpContext.Object;
             builder.RegisterInstance(httpContext).As<Mock<HttpContextBase>>();
             builder.RegisterInstance(httpContextObject).As<HttpContextBase>();
+            builder.RegisterType<ConfigurationRepository>().As<IConfigurationRepository>();
             builder.RegisterType<RedmineRepository>().As<IRedmineRepository>();
 
             var container = builder.Build();
