ae69c8802e3d675e07f2065d8c2c7f51d6ff78c9
julie <julie@PR2C4R3Y2015>
2017-03-20 08:59:39 +0530

    Read app credentials fom seed data. Fixes#284

---
+++ Slack.Automation/Promact.Core.Repository/AppCredentialRepository/IAppCredentialRepository.cs
@@ -0,0 +1,22 @@
+﻿using Promact.Erp.DomainModel.Models;
+using System.Threading.Tasks;
+
+namespace Promact.Core.Repository.AppCredentialRepository
+{
+    public interface IAppCredentialRepository
+    {
+        /// <summary>
+        /// Get the app credentials of the given module - JJ
+        /// </summary>
+        /// <param name="module"></param>
+        /// <returns>object of AppCredential</returns>
+        Task<AppCredential> FetchAppCredentialByModule(string module);
+
+        /// <summary>
+        /// Adds the app's credentials to the database - JJ
+        /// </summary>
+        /// <param name="appCredential"></param>
+        /// <returns></returns>
+        Task<int> AddAppCredentialAsync(AppCredential appCredential);
+    }
+}
