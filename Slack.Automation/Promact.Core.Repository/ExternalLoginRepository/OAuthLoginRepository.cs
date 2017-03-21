6fa78898305f19aa5c2de31f3aff039f522003b7
julie <julie@PR2C4R3Y2015>
2017-03-21 12:58:02 +0530

    removed exception. Fixes #282

--- Slack.Automation/Promact.Core.Repository/ExternalLoginRepository/OAuthLoginRepository.cs
+++ Slack.Automation/Promact.Core.Repository/ExternalLoginRepository/OAuthLoginRepository.cs
@@ -182,8 +182,6 @@ namespace Promact.Core.Repository.ExternalLoginRepository
                                     await AddChannelGroupAsync(channel);
                                 }
                             }
-                            else
-                                throw new SlackAuthorizeException(_stringConstant.SlackAuthError + channels.ErrorMessage);
                             _logger.Info("Slack User Id  : " + (await _userManager.FindByEmailAsync(applicationUser.Email)).SlackUserId);
                             //the public groups' details
                             string groupDetailsResponse = await _httpClientService.GetAsync(_stringConstant.SlackGroupListUrl, detailsRequest, null);
@@ -199,8 +197,6 @@ namespace Promact.Core.Repository.ExternalLoginRepository
                                     _logger.Debug("Slack User Id  : " + (await _userManager.FindByEmailAsync(applicationUser.Email)).SlackUserId);
                                 }
                             }
-                            else
-                                throw new SlackAuthorizeException(_stringConstant.SlackAuthError + groups.ErrorMessage);
                         }
                         else
                             throw new SlackAuthorizeException(string.Format(_stringConstant.NotInSlackOrNotExpectedUser, slackUserDetails.Profile.Email));
