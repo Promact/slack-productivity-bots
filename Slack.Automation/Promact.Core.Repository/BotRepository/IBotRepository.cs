9b38a73299b7b3d4ef9d3d3a96ad1c280d77c31b
Siddhartha Shaw <siddhartha@promactinfo.com>
2017-03-21 18:45:43 +0530

    AppCredentialRepository.cs - added a method to Clear BotToken By Module IAppCredentialRepository.cs - added a method to Clear BotToken By Module BotRepository.cs - added repository to handle task mail and scrum bot IBotRepository.cs - added repository to handle task mail and scrum bot ConfigurationRepository.cs - added functionality to turn off bot by module name OAuthLoginRepository.cs - added functionality to turn on bot by module name packages.config - Added slackAPI AutofacConfig.cs(Test) - register BotRepository AutofacConfig.cs - register BotRepository Global.asax.cs - remove bot class resolving and added IBotRepository and turn on bot on 1st time run
    Fixes - #282

---
+++ Slack.Automation/Promact.Core.Repository/BotRepository/IBotRepository.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Promact.Core.Repository.BotRepository
+{
+    public interface IBotRepository
+    {
+        /// <summary>
+        /// Method to return on bot by module name
+        /// </summary>
+        /// <param name="module">module name</param>
+        Task TurnOnBot(string module);
+
+        /// <summary>
+        /// Method to return off bot by module name
+        /// </summary>
+        /// <param name="module">module name</param>
+        Task TurnOffBot(string module);
+    }
+}
