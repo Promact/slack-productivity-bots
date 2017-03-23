177c1a1e19d7577d54c63bfaca84863b59c47b99
julie <julie@PR2C4R3Y2015>
2017-03-23 16:14:11 +0530

    Updated method. Fixes#282

--- Slack.Automation/Promact.Erp.Web/BotStartUp.cs
+++ Slack.Automation/Promact.Erp.Web/BotStartUp.cs
@@ -14,8 +14,8 @@ namespace Promact.Erp.Web
             IAppCredentialRepository appCredential = container.Resolve<IAppCredentialRepository>();
             ITaskMailBotRepository taskMailBotRepository = container.Resolve<ITaskMailBotRepository>();
             IScrumRepository scrumBotRepository = container.Resolve<IScrumRepository>();
-            taskMailBotRepository.StartAndConnectTaskMailBot((await appCredential.FetchAppCredentialByModule(stringConstant.TaskModule)).BotToken);
-            scrumBotRepository.StartAndConnectScrumBot((await appCredential.FetchAppCredentialByModule(stringConstant.Scrum)).BotToken);
+            taskMailBotRepository.StartAndConnectTaskMailBot((await appCredential.FetchAppCredentialByModuleAsync(stringConstant.TaskModule)).BotToken);
+            scrumBotRepository.StartAndConnectScrumBot((await appCredential.FetchAppCredentialByModuleAsync(stringConstant.Scrum)).BotToken);
         }
     }
 }
