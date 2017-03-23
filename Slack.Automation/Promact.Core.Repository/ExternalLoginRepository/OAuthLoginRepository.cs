3ffec1c78a6f3bcda75cd55cc7adbfeaa393a63a
julie <julie@PR2C4R3Y2015>
2017-03-23 14:47:42 +0530

    BotUserId fetched. Fixes#282

--- Slack.Automation/Promact.Core.Repository/ExternalLoginRepository/OAuthLoginRepository.cs
+++ Slack.Automation/Promact.Core.Repository/ExternalLoginRepository/OAuthLoginRepository.cs
@@ -122,7 +122,8 @@ namespace Promact.Core.Repository.ExternalLoginRepository
                 string slackOAuthResponse = await _httpClientService.GetAsync(_stringConstant.OAuthAcessUrl, slackOAuthRequest, null);
                 SlackOAuthResponse slackOAuth = JsonConvert.DeserializeObject<SlackOAuthResponse>(slackOAuthResponse);
                 appCredential.IsSelected = false;
-                appCredential.BotToken = slackOAuth.Bot.BotAccessToken;
+                appCredential.BotToken = slackOAuth?.Bot?.BotAccessToken;
+                appCredential.BotUserId = slackOAuth?.Bot?.BotUserId;
                 await _appCredentialRepository.AddUpdateAppCredentialAsync(appCredential);
                 await StartBotByModuleAsync(appCredential.Module);
                 _logger.Info("slackOAuth UserID" + slackOAuth.UserId);
