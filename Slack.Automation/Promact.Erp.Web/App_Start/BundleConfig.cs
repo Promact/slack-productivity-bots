65f940b65d8e0a29f64e91988a0aabfa68f494d4
ankit <ankit@PR1C2R4Y2015>
2017-04-28 16:32:45 +0530

    updated css file and also updated font-awesome.min.css file and also updated AfterLogin and Index.cshtml file Fixes #334

--- Slack.Automation/Promact.Erp.Web/App_Start/BundleConfig.cs
+++ Slack.Automation/Promact.Erp.Web/App_Start/BundleConfig.cs
@@ -6,26 +6,12 @@ namespace Promact.Erp.Web
     {
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
+            bundles.Add(new StyleBundle("~/bundles/css").Include(
                 "~/Content/css/font-lineicons.css",
                 "~/Content/Site.css"));
 
-            bundles.Add(new StyleBundle("~/Content/afterlogin").Include(
+            bundles.Add(new StyleBundle("~/bundles/afterlogin").Include(
                "~/Content/css/font-awesome.min.css",
                "~/Content/css/custom.min.css",
                "~/Content/css/erp-custom.css"));
@@ -33,7 +19,6 @@ namespace Promact.Erp.Web
             bundles.Add(new ScriptBundle("~/bundles/customjs").Include(
                  "~/Content/js/icheck.js",
                 "~/Content/js/custom.js"));
-            BundleTable.EnableOptimizations = false;
         }
     }
 }
